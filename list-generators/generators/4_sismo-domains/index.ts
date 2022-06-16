b61f6331f8cc402c22a5fb226467a4e046d61d5f
Mandervelde Benjamin <36704171+Kakktuss@users.noreply.github.com>
2022-06-16 11:18:17 +0200

    feat: add Sismo domains list generator
    * feat: add sismo domains list generator related to the sismo subgraph

--- list-generators/generators/4_sismo-domains/index.ts
+++ list-generators/generators/4_sismo-domains/index.ts
@@ -1,21 +1,49 @@
-import { ValueType, Tags } from "../../../src/list";
+import { gql } from "graphql-request";
+import { Tags, ValueType } from "../../../src/list";
 import {
+  GenerationFrequency,
   GeneratorContext,
   ListGenerator,
-  GenerationFrequency,
 } from "../../../src/list-generator";
 import { List } from "../../../src/list/list";
+import { dataProviders } from "../../helpers/providers";
 
 export default new ListGenerator({
   id: 4,
   name: "sismo-domains",
   generate: async (context: GeneratorContext): Promise<List> => {
     // This list is constituted by all the users who have a sismo.eth domain
+    const subgraphHostedServiceProvider =
+      new dataProviders.SubgraphHostedServiceProvider({
+        url: "https://api.thegraph.com/subgraphs/name/sismo-core/sismo-dao",
+      });
+
+    type ENSOwner = { owner: { id: string } };
+
+    const sismoDomainOwners = await subgraphHostedServiceProvider.query<{
+      domains: ENSOwner[];
+    }>(
+      gql`
+        query getAllSismoDomainOwners {
+          domains {
+            owner {
+              id
+            }
+          }
+        }
+      `
+    );
+
+    const fetchedData: { [address: string]: number } = {};
+
+    for (const domain of sismoDomainOwners.domains.map((d) => d.owner.id)) {
+      fetchedData[domain] = (fetchedData[domain] ?? 0) + 1;
+    }
 
     // construct your list
     return new List({
       generationDate: new Date(context.timestamp),
-      data: { "0x1": 1 }, // TODO replace when provider ready
+      data: fetchedData,
       valueType: ValueType.Score,
       tags: [Tags.Mainnet, Tags.ENS, Tags.User],
     });
