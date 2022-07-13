4d2e0677f27fe45bd82fe9c75fb152f6230dd46b
Benjamin Mandervelde <benjaminmandervelde@protonmail.com>
2022-07-13 21:25:37 +0200

    feat: add ethereum event attenders

--- group-generators/generators/ethereum-event-attenders/index.ts
+++ group-generators/generators/ethereum-event-attenders/index.ts
@@ -2,25 +2,27 @@ import { Group, Tags, ValueType } from "../../../src/group";
 import {
   GenerationFrequency,
   GeneratorContext,
-  GroupGenerator
+  GroupGenerator,
 } from "../../../src/group-generator";
 import { dataProviders } from "../../helpers/providers";
 import { PoapChainTarget } from "../../helpers/providers/poap/types";
 
 export default new GroupGenerator({
-  name: "sismo-diggers",
+  name: "ethereum-event-attenders",
   generate: async (context: GeneratorContext): Promise<Group> => {
     // This group is constituted by all the users who have a sismo poap
     // of the following event:
 
-    const poapProvider = new dataProviders.PoapSubgraphProvider({ chainTarget: PoapChainTarget.XDai });
+    const poapProvider = new dataProviders.PoapCrossChainSubgraphProvider({
+      chainTargets: [PoapChainTarget.XDai, PoapChainTarget.EthereumMainnet],
+    });
 
     const zikiPoapOwners = await poapProvider.queryEventsTokenOwners({
       eventIds: [
-        37527 /* Ziki Testers */, 39515 /* Ziki Artists */,
-        39651 /* Ziki Community Managers  */, 39654 /* Ziki Data Analysts */,
-        39655 /* Ziki copywriters */, 39657 /* Ziki cryptographers */,
-        39660 /* Ziki Data creators */,
+        3, 71, 15, 25, 39, 40, 41, 61, 4, 5, 11, 12, 7, 1, 9, 17, 6, 215, 217,
+        23, 14, 26, 28, 27, 43, 60, 69, 84, 80, 94, 895, 123, 126, 12, 132, 131,
+        179, 190, 189, 213, 357, 328, 350, 304, 303, 302, 305, 412, 510, 511,
+        452, 553,
       ],
     });
 
