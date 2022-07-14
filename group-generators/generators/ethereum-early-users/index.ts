be85bb5728c662090304c9ebf1355d093fb18b69
leo Sayous <leo@sismo.io>
2022-07-14 16:47:35 +0200

    feat: add group filters

--- group-generators/generators/ethereum-early-users/index.ts
+++ group-generators/generators/ethereum-early-users/index.ts
@@ -6,6 +6,7 @@ import {
 } from "../../../src/group-generator";
 import { Group } from "../../../src/group";
 import BigQueryProvider from "../../helpers/providers/big-query/big-query";
+import BigQueryHelper from "../../helpers/providers/big-query/helper";
 
 /*
   Ethereum Power Users is a group constituted by the top 500 000 Ethereum accounts 
@@ -19,13 +20,17 @@ export default new GroupGenerator({
     with transactions as (
       select from_address as address, count(*) as nb_transaction from \`bigquery-public-data.crypto_ethereum.transactions\` 
       where  block_timestamp < '2018-01-01'
+      AND ${BigQueryHelper.excludeExchangeAddresses()}
+      AND ${BigQueryHelper.excludeFunction("transfer(address,address,uint256)")}
       group by from_address
     )
     select address, nb_transaction as value
     from transactions
     order by nb_transaction DESC
-    limit 500000;
+    limit 100000;
     `;
+    console.log(query);
+    process.exit(0);
     const mostTransactionsUsers = await bigQueryProvider.fetch(query);
     return new Group({
       generationDate: new Date(context.timestamp),
