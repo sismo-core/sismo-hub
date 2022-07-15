aa0fc3a72bec1fb4a27ae8340614f6c5c8d883ba
Léo sayous <leo@sismo.io>
2022-07-15 11:35:16 +0200

    feat: add group filters helpers (#25)
    * feat: add group filters
    
    * feat: add queries construction
    
    * fix: groups
    
    Co-authored-by: Benjamin Mandervelde <benjaminmandervelde@protonmail.com>

--- group-generators/generators/ethereum-power-users/index.ts
+++ group-generators/generators/ethereum-power-users/index.ts
@@ -1,11 +1,11 @@
-import { ValueType, Tags } from "../../../src/group";
+import { Group, Tags, ValueType } from "../../../src/group";
 import {
   GenerationFrequency,
   GeneratorContext,
   GroupGenerator,
 } from "../../../src/group-generator";
-import { Group } from "../../../src/group";
 import BigQueryProvider from "../../helpers/providers/big-query/big-query";
+import BigQueryHelper from "../../helpers/providers/big-query/helper";
 
 /*
   Ethereum Power Users is a group constituted by the top 500 000 Ethereum accounts 
@@ -18,12 +18,25 @@ export default new GroupGenerator({
     const query = `
     with transactions as (
       select from_address as address, count(*) as nb_transaction from \`bigquery-public-data.crypto_ethereum.transactions\` 
+      WHERE ${BigQueryHelper.excludeExchangeAddresses()}
+      AND ${BigQueryHelper.excludeReceiptStatusNull()}
+      AND ${BigQueryHelper.excludeFunction("transfer(address,uint256)")}
+      AND ${BigQueryHelper.excludeFunction(
+        "safeTransferFrom(address,address,uint256,bytes)"
+      )}
+      AND ${BigQueryHelper.excludeFunction(
+        "safeTransferFrom(address,address,uint256)"
+      )}
+      AND ${BigQueryHelper.excludeFunction(
+        "transferFrom(address,address,uint256)"
+      )}
+      AND ${BigQueryHelper.excludeTransfer()}
       group by from_address
     )
     select address, nb_transaction as value
     from transactions
     order by nb_transaction DESC
-    limit 500000;
+    limit 10000;
     `;
     const mostTransactionsUsers = await bigQueryProvider.fetch(query);
     return new Group({
