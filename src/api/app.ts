123cb833ffc414e7619a579609c5157e8ab749ce
Nicolas Geniteau <nicolas.geniteau@gmail.com>
2022-07-17 15:06:45 +0200

    feat: add group generator api

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import groups from "../topics/group/group.api";
+import groupGenerators from "../topics/group-generator/group-generator.api";
 
 const app = express();
 
 app.use("/groups", groups);
+app.use("/group-generators", groupGenerators);
 
 export default app;
