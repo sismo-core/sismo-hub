da966e550456252bc34785ad6a650de9ce466d6a
Benjamin Mandervelde <benjaminmandervelde@protonmail.com>
2022-07-18 12:54:28 +0200

    feat: add attester helpers according to the new structure

--- src/topics/attester/attester.helper.ts
+++ src/topics/attester/attester.helper.ts
@@ -1,28 +1,57 @@
-import { getNetworkAllAttesters, getNetworkAttester } from "../../../attesters";
-import { ConstructAttesterFn, ConstructedAttester } from "./types";
+import attesters from "../../../attesters";
+import { AttesterNetwork, ConstructedAttester } from "./types";
+
+const asyncFilter = async (arr: Array<any>, predicate: any) => {
+  const results = await Promise.all(arr.map(predicate));
+  return arr.filter((_, i) => results[i]);
+};
+
+export type ConstructedNetworkAttesters = {
+  [key: string]: ConstructedAttester;
+};
+
+export type AllConstructedAttesters = {
+  [key: string]: ConstructedNetworkAttesters;
+};
 
 export async function getConstructedNetworkAttesters(
-  network: string
-): Promise<Array<ConstructedAttester | undefined>> {
-  const networkAttesters: Array<ConstructAttesterFn | undefined> =
-    await getNetworkAllAttesters(network);
-
-  return await Promise.all(
-    networkAttesters.map(
-      async (networkAttester: ConstructAttesterFn | undefined) => {
-        return networkAttester
-          ? (await networkAttester())?.compute()
-          : undefined;
-      }
-    )
+  network: AttesterNetwork
+): Promise<ConstructedNetworkAttesters> {
+  const constructedNetworkAttesters: ConstructedAttester[] = await Promise.all(
+    (
+      await asyncFilter(attesters, async (attester: any) => {
+        return (
+          (await attester())?.availableNetworkConfigurations[network] !==
+          undefined
+        );
+      })
+    ).map(async (attester) => {
+      return (await attester()).getForNetwork(network).compute();
+    })
   );
+
+  return constructedNetworkAttesters
+    .filter((attester) => attester.configuration)
+    .reduce((acc, curr) => ({ ...acc, [curr.name]: curr }), {});
 }
 
-export async function getConstructedNetworkAttester(
-  network: string,
-  attester: string
-): Promise<ConstructedAttester | undefined> {
-  const networkAttester = await getNetworkAttester(network, attester);
+export async function getConstructedAttesters(): Promise<AllConstructedAttesters> {
+  const allAttesters: AllConstructedAttesters = {};
 
-  return networkAttester ? (await networkAttester())?.compute() : undefined;
+  for (const networkAttester in AttesterNetwork) {
+    allAttesters[networkAttester.toLowerCase()] =
+      await getConstructedNetworkAttesters(
+        networkAttester.toLowerCase() as AttesterNetwork
+      );
+  }
+
+  return allAttesters;
+}
+
+export async function getConstructNetworkAttester(
+  network: AttesterNetwork,
+  name: string
+): Promise<ConstructedAttester> {
+  const attesters = await getConstructedNetworkAttesters(network);
+  return attesters[name] ?? {};
 }
