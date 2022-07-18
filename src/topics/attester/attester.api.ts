4cd2ecea1980ce886c2f7ff1ffa8b610c05e8556
Benjamin Mandervelde <benjaminmandervelde@protonmail.com>
2022-07-18 12:54:46 +0200

    feat: add new attester api

--- src/topics/attester/attester.api.ts
+++ src/topics/attester/attester.api.ts
@@ -1,21 +1,27 @@
 import { Router } from "express";
 import {
-  getConstructedNetworkAttester,
+  getConstructedAttesters,
   getConstructedNetworkAttesters,
+  getConstructNetworkAttester,
 } from "./attester.helper";
+import { AttesterNetwork } from "./types";
 
 const router = Router();
 
+router.get("/", async (req, res) => {
+  res.json(await getConstructedAttesters());
+});
+
 router.get("/:network", async (req, res) => {
   const { network } = req.params;
-
-  res.send(JSON.stringify(getConstructedNetworkAttesters(network)));
+  res.json(await getConstructedNetworkAttesters(network as AttesterNetwork));
 });
 
 router.get("/:network/:attester", async (req, res) => {
   const { network, attester } = req.params;
-
-  res.send(JSON.stringify(getConstructedNetworkAttester(network, attester)));
+  res.json(
+    await getConstructNetworkAttester(network as AttesterNetwork, attester)
+  );
 });
 
 export default router;
