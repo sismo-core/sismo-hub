c42f0cded036ca440c6da992004f3c92fda0a14e
Nicolas Geniteau <nicolas.geniteau@gmail.com>
2022-07-22 16:23:53 +0200

    refactor: replace express by fastify

--- src/topics/group-generator/group-generator.api.test.ts
+++ src/topics/group-generator/group-generator.api.test.ts
@@ -1,21 +1,25 @@
 import request from "supertest";
-import app from "../../api/app";
-import GroupGeneratorLibrary from "./group-generator-library";
+import { getFastify } from "../../api/app";
 import TestGroupGenerator from "./test-group-generator";
+import { GroupGenerator } from "./group-generator";
+import { FastifyInstance } from "fastify";
 
 describe("test groups generator api", () => {
+  let fastify: FastifyInstance;
   let testGroupGenerator: TestGroupGenerator;
+  let groupGenerators: { [name: string]: GroupGenerator };
 
   beforeAll(async () => {
     testGroupGenerator = new TestGroupGenerator();
-
-    GroupGeneratorLibrary.init({
+    groupGenerators = {
       "example-generator": testGroupGenerator,
-    });
+    };
+    fastify = getFastify(false, { groupGenerators });
+    await fastify.ready();
   });
 
   test("Should get example-generator", async () => {
-    const response = await request(app).get(`/group-generators`);
+    const response = await request(fastify.server).get(`/group-generators`);
     expect(response.statusCode).toBe(200);
     expect(response.body.items).toHaveLength(1);
     expect(response.body.items[0].name).toBe("example-generator");
--- src/topics/group/group.api.ts
+++ src/topics/group/group.api.ts
@@ -1,13 +1,13 @@
-import { Router } from "express";
+import { FastifyInstance, FastifyRequest } from "fastify";
 import { GroupSearch, ValueType, Tags } from "./group.types";
 import { Group } from "./group";
 
-const router = Router();
-
-type GroupQueryType = {
-  groupName?: string;
-  latest?: boolean;
-};
+type GroupRequestType = FastifyRequest<{
+  Querystring: {
+    groupName?: string;
+    latest?: boolean;
+  };
+}>;
 
 type GroupAPIType = {
   name: string;
@@ -27,36 +27,37 @@ const serialize = (group: Group): GroupAPIType => {
   };
 };
 
-router.get("/", async (req, res) => {
-  const query: GroupQueryType = req.query;
-  if (!query.groupName) {
-    res.status(400);
-    res.json({
-      error: "invalidRequest",
-      message: "groupName is required",
-    });
-    return;
-  }
-  const groupSearch: GroupSearch = {
-    groupName: query.groupName,
-    latest: query.latest,
-  };
-  res.json({
-    items: (await Group.store.search(groupSearch)).map((group) =>
-      serialize(group)
-    ),
+const routes = async (fastify: FastifyInstance) => {
+  fastify.get("/groups", async (request: GroupRequestType, reply) => {
+    const query = request.query;
+    if (!query.groupName) {
+      reply.code(400).send({
+        error: "invalidRequest",
+        message: "groupName is required",
+      });
+      return;
+    }
+    const groupSearch: GroupSearch = {
+      groupName: query.groupName,
+      latest: query.latest,
+    };
+    return {
+      items: (await Group.store.search(groupSearch)).map((group) =>
+        serialize(group)
+      ),
+    };
   });
-});
 
-router.get("/latests", async (req, res) => {
-  const groups = await Group.store.latests();
-  const items: { [groupName: string]: GroupAPIType } = {};
-  for (const groupName in groups) {
-    items[groupName] = serialize(groups[groupName]);
-  }
-  res.json({
-    items: items,
+  fastify.get("/groups/latests", async () => {
+    const groups = await Group.store.latests();
+    const items: { [groupName: string]: GroupAPIType } = {};
+    for (const groupName in groups) {
+      items[groupName] = serialize(groups[groupName]);
+    }
+    return {
+      items: items,
+    };
   });
-});
+};
 
-export default router;
+export default routes;
