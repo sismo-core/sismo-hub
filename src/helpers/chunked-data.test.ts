298ad54cace2bbe877b0e110c747163e41b61d4a
Nicolas Geniteau <nicolas.geniteau@gmail.com>
2022-07-26 12:59:17 +0200

    feat: add ChunkedData helper

---
+++ src/helpers/chunked-data.test.ts
@@ -0,0 +1,77 @@
+import { ChunkedData, ChunkType } from "./";
+
+describe("test chunked data", () => {
+  const exampleData = {
+    key3: 3,
+    key1: 1,
+    key2: 2,
+  };
+
+  it("Should create a chunked data and get valid total chunk", async () => {
+    expect(new ChunkedData<number>(exampleData, 1).totalChunks).toBe(3);
+    expect(new ChunkedData<number>(exampleData, 2).totalChunks).toBe(2);
+  });
+
+  it("Should create a chunked data and get keys sorted", async () => {
+    expect(new ChunkedData<number>(exampleData, 2).keys).toEqual([
+      "key1",
+      "key2",
+      "key3",
+    ]);
+  });
+
+  it("Should create a chunked data and get valid chunks 1/2", async () => {
+    const chunkData = new ChunkedData<number>(exampleData, 2);
+    const chunks: ChunkType<number>[] = [];
+
+    for (const chunk of chunkData.iterate()) {
+      chunks.push(chunk);
+    }
+    expect(chunks).toHaveLength(2);
+    expect(chunks[0]).toEqual({
+      chunkNumber: 0,
+      totalChunks: 2,
+      min: "key1",
+      max: "key2",
+      data: { key1: 1, key2: 2 },
+    });
+    expect(chunks[1]).toEqual({
+      chunkNumber: 1,
+      totalChunks: 2,
+      min: "key3",
+      max: "key3",
+      data: { key3: 3 },
+    });
+  });
+
+  it("Should create a chunked data and get valid chunks 2/2", async () => {
+    const chunkData = new ChunkedData<number>(exampleData, 1);
+    const chunks: ChunkType<number>[] = [];
+
+    for (const chunk of chunkData.iterate()) {
+      chunks.push(chunk);
+    }
+    expect(chunks).toHaveLength(3);
+    expect(chunks[0]).toEqual({
+      chunkNumber: 0,
+      totalChunks: 3,
+      min: "key1",
+      max: "key1",
+      data: { key1: 1 },
+    });
+    expect(chunks[1]).toEqual({
+      chunkNumber: 1,
+      totalChunks: 3,
+      min: "key2",
+      max: "key2",
+      data: { key2: 2 },
+    });
+    expect(chunks[2]).toEqual({
+      chunkNumber: 2,
+      totalChunks: 3,
+      min: "key3",
+      max: "key3",
+      data: { key3: 3 },
+    });
+  });
+});
---
+++ src/helpers/chunked-data.ts
@@ -0,0 +1,46 @@
+export type ChunkType<T> = {
+  chunkNumber: number;
+  totalChunks: number;
+  min: string;
+  max: string;
+  data: { [key: string]: T };
+};
+
+export class ChunkedData<T> {
+  private readonly _data: { [key: string]: T };
+  public readonly keys: string[];
+  public readonly chunkSize: number;
+  public readonly totalChunks: number;
+
+  constructor(data: { [key: string]: T }, chunkSize: number) {
+    this._data = data;
+    this.keys = Object.keys(this._data).sort();
+    this.chunkSize = chunkSize;
+    this.totalChunks = Math.ceil(this.keys.length / chunkSize);
+  }
+
+  *iterate(): Generator<ChunkType<T>, void, undefined> {
+    for (
+      let chunkNumber = 0;
+      chunkNumber < this.totalChunks;
+      chunkNumber += 1
+    ) {
+      const chunkData: { [key: string]: T } = {};
+      const lastIndex =
+        Math.min(
+          chunkNumber * this.chunkSize + this.chunkSize,
+          this.keys.length
+        ) - 1;
+      for (let i = chunkNumber * this.chunkSize; i <= lastIndex; i += 1) {
+        chunkData[this.keys[i]] = this._data[this.keys[i]];
+      }
+      yield {
+        chunkNumber: chunkNumber,
+        totalChunks: this.totalChunks,
+        min: this.keys[chunkNumber * this.chunkSize],
+        max: this.keys[lastIndex],
+        data: chunkData,
+      };
+    }
+  }
+}
