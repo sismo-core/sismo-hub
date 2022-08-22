c1fe7aa949f9e98b4b6cdad87a8ee1ca10e3c335
Nicolas Geniteau <nicolas.geniteau@gmail.com>
2022-08-22 18:58:51 +0200

    feat: add openapi schema validation

--- src/cli/program.ts
+++ src/cli/program.ts
@@ -1,10 +1,11 @@
 import { Command } from "commander";
-import { apiCmd } from "api";
+import { apiCmd, openApiCmd } from "api";
 import { computeAttesterCmd } from "topics/attester/attester.commands";
 import { generateGroupCmd } from "topics/group-generator/group-generator.commands";
 
 export const program = new Command();
 
 program.addCommand(apiCmd);
+program.addCommand(openApiCmd);
 program.addCommand(generateGroupCmd);
 program.addCommand(computeAttesterCmd);
