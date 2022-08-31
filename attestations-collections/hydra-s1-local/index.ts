16bc73c8f875676c0eb22c801f8041b71223147e
bigq <bigq@MacBook-Pro-de-bigq.local>
2022-08-31 15:50:52 +0200

    feature: prepare local env tuto

--- attestations-collections/hydra-s1-local/index.ts
+++ attestations-collections/hydra-s1-local/index.ts
@@ -17,7 +17,21 @@ export const hydraS1LocalAttester = generateHydraS1Attester(
       {
         internalCollectionId: 0,
         groupFetcher: async (groupStore) => [
-          await groupStore.latest("sismo-contributors"),
+          await groupStore.latest("local-group"),
+        ],
+      },
+      // Fake Masquerade 
+      {
+        internalCollectionId: 1,
+        groupFetcher: async (groupStore) => [
+          await groupStore.latest("local-group"),
+        ],
+      },
+      // Fake ethereum power users
+      {
+        internalCollectionId: 2,
+        groupFetcher: async (groupStore) => [
+          await groupStore.latest("local-group"),
         ],
       },
     ],
@@ -34,6 +48,23 @@ export const hydraS1LocalBadges: BadgesCollection = {
       name: "ZK Badge: Sismo Contributor",
       description: "ZK Badge received by Sismo contributors",
       image: "sismo_digger.svg",
+      attributes: {},
+      requirements: [],
+    },
+    {
+      internalCollectionId: 1,
+      name: "Sismo Masquerade Bloomer ZK Badge",
+      description:
+        "ZK Badge owned by @masquerade.lens and @sismo.lens Lens followers",
+      image: "sismo_masquerade_bloomers.svg",
+      attributes: {},
+      requirements: [],
+    },
+    {
+      internalCollectionId: 2,
+      name: "Ethereum Power Users ZK Badge",
+      description: "ZK Badge owned by the most active users of Ethereum",
+      image: "ethereum_power_users.svg",
       attributes: {},
       requirements: [],
     },
