1501626674218a3440dd813666c119c947b6931a
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2022-09-05 20:42:04 +0530

    feat: rest api provider

---
+++ group-generators/helpers/providers/rest-api/types.ts
@@ -0,0 +1,10 @@
+export type ApiConfig = {
+  // `url` is the api URL that will be used for the request
+  url: string;
+
+  // `method` is the request method to be used when making the request
+  method: string;
+
+  // `data` is the data to be sent as the request body
+  data: object;
+};
