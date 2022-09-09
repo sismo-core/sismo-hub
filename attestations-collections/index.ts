9891094c11b7e2cad7ef0a25a479621cb77f86d8
bigq <yum0e@protonmail.com>
2022-09-09 16:37:51 +0200

    fix: add rinkeby attester

--- attestations-collections/index.ts
+++ attestations-collections/index.ts
@@ -7,6 +7,10 @@ import {
   hydraS1SimpleAttester as hydraS1SimpleAttesterPolygonPlayground,
   hydraS1SimpleBadges as hydraS1SimplePolygonPlaygroundBadges,
 } from "./hydra-s1-simple-polygon-playground";
+import {
+  hydraS1SimpleAttester as hydraS1SimpleRinkeby,
+  hydraS1SimpleBadges as hydraS1SimpleRinkebyBadges,
+} from "./hydra-s1-simple-rinkeby";
 import { AttestersLibrary } from "topics/attester";
 import { BadgesCollection } from "topics/badge";
 
@@ -14,10 +18,12 @@ export const badges: BadgesCollection[] = [
   hydraS1LocalBadges,
   hydraS1SimplePolygonBadges,
   hydraS1SimplePolygonPlaygroundBadges,
+  hydraS1SimpleRinkebyBadges,
 ];
 
 export const attesters: AttestersLibrary = {
   "hydra-s1-local": hydraS1LocalAttester,
   "hydra-s1-simple-polygon": hydraS1SimpleAttesterPolygon,
   "hydra-s1-simple-polygon-playground": hydraS1SimpleAttesterPolygonPlayground,
+  "hydra-s1-simple-rinkeby": hydraS1SimpleRinkeby,
 };
