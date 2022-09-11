0fcdc595941cd5d8b11e473f9d4c61436056e819
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2022-09-11 15:42:34 +0530

    added the Method type to the 'method' param

--- group-generators/helpers/providers/rest-api/types.ts
+++ group-generators/helpers/providers/rest-api/types.ts
@@ -1,9 +1,11 @@
+import { Method } from "axios";
+
 export type ApiConfig = {
   // `url` is the api URL that will be used for the request
   url: string;
 
   // `method` is the request method to be used when making the request
-  method: string;
+  method: Method;
 
   // `data` is the data to be sent as the request body
   data: object;
