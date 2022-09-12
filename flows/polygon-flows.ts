7bb2f68f6a9eefb7fcc0e2073b8421cec173b09d
leo Sayous <leo@sismo.io>
2022-09-12 10:57:58 +0200

    feat: add synaps flow

--- flows/polygon-flows.ts
+++ flows/polygon-flows.ts
@@ -1,6 +1,7 @@
 import {
   hydraS1SimpleAttester,
   hydraS1SimpleBadges,
+  pythia1SimpleBadges,
 } from "@attestations-collections/polygon";
 import { Network } from "topics/attester";
 import { Flow } from "topics/flow";
@@ -58,6 +59,25 @@ export const polygonFlows: Flow[] = [
     congratulationTexts: [
       "You can now prove that you are a human",
       "and access human-gated services!",
+    ],
+  },
+  {
+    path: "synaps-liveness",
+    attester: "pythia-1-simple",
+    network: Network.Polygon,
+    attesterType: "pythia-1",
+    badgesCollection: pythia1SimpleBadges,
+    badgesInternalCollectionsIds: [0],
+    title: "Synaps",
+    logoUrl: null,
+    subtitle: "Prove you are not a bot",
+    onboardingDescription:
+      "This ZK Badge is an attestation that you are a human and not a bot. It is used by diverse applications (e.g Lens) as a sybil resistant tool.",
+    ctaLabel: "See your badge",
+    ctaUrl: "",
+    congratulationTexts: [
+      "You can now prove that you are",
+      "not a bot by showing this badge!",
     ],
   },
 ];
