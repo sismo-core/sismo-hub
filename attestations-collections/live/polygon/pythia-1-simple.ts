a3f01dec210345495b1fcceb904c15e488dfb5d6
leo Sayous <leo@sismo.io>
2022-09-13 13:11:38 +0200

    feat: disable liveness flow for live env

--- attestations-collections/live/polygon/pythia-1-simple.ts
+++ attestations-collections/live/polygon/pythia-1-simple.ts
@@ -6,14 +6,14 @@ export const pythia1SimpleBadges: BadgesCollection = {
     [Network.Polygon]: 30000001,
   },
   badges: [
-    {
-      internalCollectionId: 0,
-      name: "Synaps Liveness ZK Badge",
-      description:
-        "ZK Badge owned by users that proved their liveness with Synaps",
-      image: "synaps_liveness.svg",
-      attributes: {},
-      requirements: ["Prove your Liveness with Synaps"],
-    },
+    // {
+    //   internalCollectionId: 0,
+    //   name: "Synaps Liveness ZK Badge",
+    //   description:
+    //     "ZK Badge owned by users that proved their liveness with Synaps",
+    //   image: "synaps_liveness.svg",
+    //   attributes: {},
+    //   requirements: ["Prove your Liveness with Synaps"],
+    // },
   ],
 };
