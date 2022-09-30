8bd64b7f72feaf3c2a6ddbf434203c0f8069cfc4
Léo sayous <leo@sismo.io>
2022-09-30 13:45:36 +0200

    feat: change martin thread and proof of humanity generation frequency (#154)

--- group-generators/generators/proof-of-humanity/index.ts
+++ group-generators/generators/proof-of-humanity/index.ts
@@ -8,7 +8,7 @@ import {
 } from "topics/group-generator";
 
 const generator: GroupGenerator = {
-  generationFrequency: GenerationFrequency.Once,
+  generationFrequency: GenerationFrequency.Weekly,
 
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
     const subgraphHostedServiceProvider =
