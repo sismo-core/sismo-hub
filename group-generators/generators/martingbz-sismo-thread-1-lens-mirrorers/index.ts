ffc4bd3744cae4bd77448e2fee760b0185c9182a
leo Sayous <leo@sismo.io>
2022-09-30 13:41:10 +0200

    feat: change martin thread and proof of humanity generation frequency

--- group-generators/generators/martingbz-sismo-thread-1-lens-mirrorers/index.ts
+++ group-generators/generators/martingbz-sismo-thread-1-lens-mirrorers/index.ts
@@ -7,7 +7,7 @@ import {
 } from "topics/group-generator";
 
 const generator: GroupGenerator = {
-    generationFrequency: GenerationFrequency.Weekly,
+    generationFrequency: GenerationFrequency.Daily,
 
     generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
         const lensProvider = new dataProviders.LensProvider();
