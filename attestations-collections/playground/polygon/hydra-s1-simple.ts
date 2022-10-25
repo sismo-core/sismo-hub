10048e6040cda2155f75d889a9c22f22c21528a9
bigq <99990370+yum0e@users.noreply.github.com>
2022-10-25 19:51:37 +0200

    feat: update badges specifications (#233)

--- attestations-collections/playground/polygon/hydra-s1-simple.ts
+++ attestations-collections/playground/polygon/hydra-s1-simple.ts
@@ -166,11 +166,11 @@ export const hydraS1SimpleAttester = generateHydraS1Attester(
       },
       // Rhino.Fi Power Users
       {
-        internalCollectionId: 88, 
+        internalCollectionId: 88,
         groupFetcher: async (groupStore) => [
-          await groupStore.latest("rhinofi-power-users"), 
+          await groupStore.latest("rhinofi-power-users"),
         ],
-      }
+      },
     ],
   }
 );
@@ -197,13 +197,14 @@ export const hydraS1SimpleBadges: BadgesCollection = {
       eligibility: {
         shortDescription:
           "Follow @sismo.lens and @masquerade.lens before July 6 2022",
-        specification: "",
+        specification:
+          "Follow @sismo.lens and @masquerade.lens before July 6 2022 on apps powered by Lens Protocol (Lenster, Orb, ..).",
       },
       links: [],
     },
     {
       internalCollectionId: 4,
-      name: "[playground] Ethereum Power Users ZK Badge",
+      name: "[playground] Ethereum Power User ZK Badge",
       description:
         "[playground] ZK Badge owned by the most active users of Ethereum",
       image: "ethereum_power_users.svg",
@@ -217,7 +218,8 @@ export const hydraS1SimpleBadges: BadgesCollection = {
       eligibility: {
         shortDescription:
           "Be part of the top 0.1% most active users on Ethereum",
-        specification: "",
+        specification:
+          "Be part of the top 50k accounts that sent the most transactions (token transfers excluded) on Ethereum between 2015 and December 31st 2016, or be part of the top 50k accounts between 2015 and December 31st 2017, or be part of the top 50k accounts between 2015 and December 31st 2018, or be part of the top 50k accounts between 2015 and December 31st 2019, or be part of the top 50k accounts between 2015 and December 31st 2020, or be part of the top 50k accounts between 2015 and December 31st 2021",
       },
       links: [],
     },
@@ -289,13 +291,19 @@ export const hydraS1SimpleBadges: BadgesCollection = {
       ],
       eligibility: {
         shortDescription: "Prove you are a human with PoH",
-        specification: "",
+        specification:
+          "Appear as a verified Proof of Humanity submission on the Proof of Humanity subgraph",
       },
       links: [
         {
           logoUrl: "",
           label: "PoH",
           url: "https://www.proofofhumanity.id/",
+        },
+        {
+          logoUrl: "",
+          label: "Subgraph",
+          url: "https://thegraph.com/explorer/subgraph?id=CvzNejNZR2UTQ66wL7miGgfWh9dmiwgTtTfgQCBvMQRE&view=Overview",
         },
       ],
     },
@@ -675,13 +683,19 @@ export const hydraS1SimpleBadges: BadgesCollection = {
       eligibility: {
         shortDescription:
           "You must have donated in the 15th round of Gitcoin Grants",
-        specification: "",
+        specification:
+          "Donated to the Gitcoin Grant Round 15 through bulkCheckout Contracts on Ethereum (0x7d655c57f71464B6f83811C55D84009Cd9f5221C), or on Polygon (0xb99080b9407436eBb2b8Fe56D45fFA47E9bb8877), or on ZKSync (0xde21f729137c5af1b01d73af1dc21effa2b8a0d6), or appear on the Gitcoin Grants Round 15 API",
       },
       links: [
         {
           logoUrl: "",
           label: "GR15",
           url: "https://gitcoin.co/grants/explorer",
+        },
+        {
+          logoUrl: "",
+          label: "Round 15 API",
+          url: "https://gitcoin.co/grants/v1/api/export_addresses/round15.json",
         },
       ],
     },
@@ -779,7 +793,7 @@ export const hydraS1SimpleBadges: BadgesCollection = {
       eligibility: {
         shortDescription: "Hold one of the POAPs from a curated list of events",
         specification:
-          "Attend EthCC4, or EthCC5, or DevCon 5, or DappCon 2019, or DevConnect Co-work space, or ETHNew York, or ETHBerlin 3, or Poap Sponsor boot @DappCon22 or met Patricio during events on December 2021 or on February 2022, or on March 2022, or on April 2022, or on May 2022, or on June 2022, or on July 2022, or on August 2022, or on September 2022",
+          "Attend EthCC4 (3695), or EthCC5 (53834), or Devcon VI (60695), or DevCon V (69), or DevConnect Co-work space (36029), or ETH New York Stacked Hacker (53425), or ETHBerlin 3 (65440), or Poap Sponsor boot @DappCon22 (63682) or met Patricio during events on December 2021 (15916) or on February 2022 (25149), or on March 2022 (30875), or on April 2022 (36528), or on May 2022 (42068), or on June 2022 (47144), or on July 2022 (53153), or on August 2022 (57318), or on September 2022 (63400)",
       },
       links: [
         {
@@ -856,7 +870,8 @@ export const hydraS1SimpleBadges: BadgesCollection = {
       eligibility: {
         shortDescription:
           "Prove that you are involved in Sismo by holding .sismo.eth ENS, a contribution POAP, or early ZK Badges.",
-        specification: "",
+        specification:
+          "Hold a .sismo.eth Sismo ENS subdomain (Sismo Genesis 0, or X, or A token), or hold a Sismo Contributor Poap (37527: User Testing, or 39515: Artists, or 39651: Community Managers, or 39654: Data Analysts, or 39655: Copywriters, or 39657: Cryptographers, or 39660: Data creators, or 54045: Ziki Run, or 66267: Contributor), or hold a 53325: Meet Sismo @ETHCC POAP, or a 48976: Sismo PreMasquerade POAP, or a 48975: Sismo Masquerade POAP, or hold a early ZK Badge (Masquerade ZK Badge, or Early User ZK Badge, or PoH ZK Badge, or a Ethereum Power User ZK Badge, or a Proof of Attendance ZK Badge, or a ENS Supporter ZK Badge, or a Gitcoin GR15 ZK Badge) or donated to the Sismo Gitcoin Grant 41, or be part of the Sismo Core team",
       },
       links: [],
     },
@@ -897,28 +912,31 @@ export const hydraS1SimpleBadges: BadgesCollection = {
       ],
     },
     {
-      internalCollectionId: 88, 
-      name: "Rhino.Fi Power User ZK Badge", 
-      description: "ZK Badge received by Rhino.Fi Power Users", 
-      image: "rhinofi-badge.svg", 
-      groupGeneratorName: "rhinofi-power-users", 
-      publicContacts: [{ 
-          type: "twitter", 
-          contact: "rhinofi" 
-        }
+      internalCollectionId: 88,
+      name: "Rhino.Fi Power User ZK Badge",
+      description: "ZK Badge received by Rhino.Fi Power Users",
+      image: "rhinofi-badge.svg",
+      groupGeneratorName: "rhinofi-power-users",
+      publicContacts: [
+        {
+          type: "twitter",
+          contact: "rhinofi",
+        },
       ],
       eligibility: {
         shortDescription: "Active users of Rhino.Fi app since May 2021",
-        specification: "You should have used Rhino.Fi app for at least 3 weeks since May 2021 with a minimum trading volume of $100 USD during those weeks",
+        specification:
+          "You should have used Rhino.Fi app for at least 3 weeks since May 2021 with a minimum trading volume of $100 USD during those weeks",
       },
       links: [
         {
-          logoUrl: "https://rhino.fi/wp-content/uploads/2021/10/rhino.fi_Primary_Logo_Grad-1.svg", 
-          label: "Rhino.Fi", 
-          url: "https://rhino.fi/" 
-        }
-      ]
-    }, 
+          logoUrl:
+            "https://rhino.fi/wp-content/uploads/2021/10/rhino.fi_Primary_Logo_Grad-1.svg",
+          label: "Rhino.Fi",
+          url: "https://rhino.fi/",
+        },
+      ],
+    },
   ],
 };
 
