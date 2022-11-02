3b4a692554ed52e1184ae7606d838631f4571271
bigq <99990370+yum0e@users.noreply.github.com>
2022-11-02 14:43:28 +0100

    fix: remove liveness badge (#282)

--- attestations-collections/staging/mumbai/pythia-1-simple.ts
+++ attestations-collections/staging/mumbai/pythia-1-simple.ts
@@ -6,30 +6,30 @@ export const pythia1SimpleBadges: BadgesCollection = {
     [Network.Mumbai]: 30000001,
   },
   badges: [
-    {
-      internalCollectionId: 0,
-      name: "Synaps Liveness ZK Badge",
-      description:
-        "ZK Badge owned by users that proved their liveness with Synaps",
-      image: "synaps_liveness.svg",
-      publicContacts: [
-        {
-          type: "twitter",
-          contact: "@sismo_eth",
-        },
-      ],
-      eligibility: {
-        shortDescription: "Prove your Liveness with Synaps",
-        specification:
-          "Prove that you are not a bot by using the Liveness verification operated by Synaps",
-      },
-      links: [
-        {
-          logoUrl: "",
-          label: "Synaps",
-          url: "https://synaps.io/",
-        },
-      ],
-    },
+    // {
+    //   internalCollectionId: 0,
+    //   name: "Synaps Liveness ZK Badge",
+    //   description:
+    //     "ZK Badge owned by users that proved their liveness with Synaps",
+    //   image: "synaps_liveness.svg",
+    //   publicContacts: [
+    //     {
+    //       type: "twitter",
+    //       contact: "@sismo_eth",
+    //     },
+    //   ],
+    //   eligibility: {
+    //     shortDescription: "Prove your Liveness with Synaps",
+    //     specification:
+    //       "Prove that you are not a bot by using the Liveness verification operated by Synaps",
+    //   },
+    //   links: [
+    //     {
+    //       logoUrl: "",
+    //       label: "Synaps",
+    //       url: "https://synaps.io/",
+    //     },
+    //   ],
+    // },
   ],
 };
