2096e09a850d7cdcf1fedafb408e114bd006aae2
bigq <yum0e@protonmail.com>
2022-11-02 14:33:10 +0100

    fix: remove liveness badge

--- flows/playground.ts
+++ flows/playground.ts
@@ -2,7 +2,6 @@ import {
   hydraS1SimpleAttester,
   hydraS1SimpleBadges,
 } from "@attestations-collections/playground/polygon/hydra-s1-simple";
-import { pythia1SimpleBadges } from "@attestations-collections/playground/polygon/pythia-1-simple";
 import { playgroundFactoryFlows } from "@flows/factory/playground-factory";
 import { Network } from "topics/attester";
 import { Flow } from "topics/flow";
@@ -73,25 +72,25 @@ export const playgroundFlows: Flow[] = [
       "and access human-gated services!",
     ],
   },
-  {
-    path: "synaps-liveness",
-    attester: "pythia-1-simple",
-    network: Network.Polygon,
-    attesterType: "pythia-1",
-    badgesCollection: pythia1SimpleBadges,
-    badgesInternalCollectionsIds: [0],
-    title: "Synaps",
-    logoUrl: null,
-    subtitle: "Prove you are not a bot",
-    onboardingDescription:
-      "This ZK Badge is an attestation that you are a human and not a bot. It is used by diverse applications (e.g Lens) as a sybil resistant tool.",
-    ctaLabel: "See your badge",
-    ctaUrl: "",
-    congratulationTexts: [
-      "You can now prove that you are",
-      "not a bot by showing this badge!",
-    ],
-  },
+  // {
+  //   path: "synaps-liveness",
+  //   attester: "pythia-1-simple",
+  //   network: Network.Polygon,
+  //   attesterType: "pythia-1",
+  //   badgesCollection: pythia1SimpleBadges,
+  //   badgesInternalCollectionsIds: [0],
+  //   title: "Synaps",
+  //   logoUrl: null,
+  //   subtitle: "Prove you are not a bot",
+  //   onboardingDescription:
+  //     "This ZK Badge is an attestation that you are a human and not a bot. It is used by diverse applications (e.g Lens) as a sybil resistant tool.",
+  //   ctaLabel: "See your badge",
+  //   ctaUrl: "",
+  //   congratulationTexts: [
+  //     "You can now prove that you are",
+  //     "not a bot by showing this badge!",
+  //   ],
+  // },
   {
     path: "proof-of-lepak-member",
     attester: hydraS1SimpleAttester.name,
@@ -438,8 +437,7 @@ export const playgroundFlows: Flow[] = [
     title: "Aztec Depositor",
     logoUrl: null,
     subtitle: "Prove that you are an Aztec user.",
-    onboardingDescription:
-      "Mint this badge to show that you are an Aztec user",
+    onboardingDescription: "Mint this badge to show that you are an Aztec user",
     ctaLabel: "Enter ZKPay.finance Discord",
     ctaUrl: "https://discord.gg/CT27dke2",
     congratulationTexts: ["Congratulations you're an Aztec user!"],
@@ -454,13 +452,11 @@ export const playgroundFlows: Flow[] = [
     title: "TimeswapLabs Early Follower",
     logoUrl: null,
     subtitle: "An early follower of TimeswapLabs on Lens",
-    onboardingDescription:
-      "",
+    onboardingDescription: "",
     ctaLabel: "Go to Timeswap Discord",
     ctaUrl: "https://discord.gg/YTUtTJfc6S",
     congratulationTexts: [
       "Congratulations you're an early follower of TimeswapLabs on Lens!",
-      
     ],
   },
 ];
