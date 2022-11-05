cca9cf47e655dc9525b7ef06c272532f19652114
bigq <yum0e@protonmail.com>
2022-11-05 15:32:42 +0100

    feat: add top twitter accounts group from hive

---
+++ group-generators/generators/twitter-ethereum-influencers/index.ts
@@ -0,0 +1,50 @@
+import { dataOperators } from "@group-generators/helpers/data-operators";
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import {
+  Tags,
+  ValueType,
+  GroupWithData,
+  AccountSource,
+  FetchedData,
+} from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Once,
+
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    const hiveProvider = new dataProviders.HiveProvider();
+
+    const twitterEthereumInfluencersPower3: FetchedData =
+      await hiveProvider.getTwitterAccountsInCluster("ethereum", 1000, 3);
+
+    const twitterEthereumInfluencersPower2: FetchedData =
+      await hiveProvider.getTwitterAccountsInCluster("ethereum", 5000, 2);
+
+    const twitterEthereumInfluencersPower1: FetchedData =
+      await hiveProvider.getTwitterAccountsInCluster("ethereum", 20000, 1);
+
+    const data: FetchedData = dataOperators.Union([
+      twitterEthereumInfluencersPower1,
+      twitterEthereumInfluencersPower2,
+      twitterEthereumInfluencersPower3,
+    ]);
+
+    return [
+      {
+        name: "twitter-ethereum-influencers",
+        timestamp: context.timestamp,
+        data,
+        accountSources: [AccountSource.TWITTER],
+        valueType: ValueType.Score,
+        tags: [Tags.User, Tags.Twitter],
+      },
+    ];
+  },
+};
+
+export default generator;
--- group-generators/helpers/data-providers/hive/index.ts
+++ group-generators/helpers/data-providers/hive/index.ts
@@ -1,6 +1,9 @@
+import readline from "readline";
+import axios from "axios";
 import { SocialAccount } from "./types";
 import { UserData } from "@group-generators/helpers/data-providers/eth-leaderboard/types";
 import { RESTProvider } from "@group-generators/helpers/data-providers/rest-api";
+import { FetchedData } from "topics/group";
 
 export class HiveProvider {
   restProvider: RESTProvider;
@@ -11,47 +14,74 @@ export class HiveProvider {
     this.restProvider = new RESTProvider();
   }
 
-  public async *getInfluencersFromClusterWithMinimumFollowers(clusterName: string, minimumNbOfFollowers: number, maxQueriedInfluencers = 10000) {
+  public async *getInfluencersFromClusterWithMinimumFollowers(
+    clusterName: string,
+    maxQueriedInfluencers = 10000,
+    minimumNbOfFollowers = 0
+  ) {
     // we have 50 results per page on Hive API
-    let pageCounter = 0
-    let lastSocialAccount: SocialAccount = 
-      { 
-        id : 0,
-        personal_rank: 0,
-        followers_count: 100000000000,
-        name: "",
-        screen_name: ""
-      }
+    let pageCounter = 0;
+    let downloadNumber = 0;
+    let lastSocialAccount: SocialAccount = {
+      id: 0,
+      personal_rank: 0,
+      followers_count: 100000000000,
+      name: "",
+      screen_name: "",
+    };
     do {
-      const res = await this.restProvider.fetchData({
+      const res = await axios({
         url: `${this.url}influence/clusters/${clusterName}/influencers/?page=${pageCounter}&sort_by=rank&sort_direction=asc`,
         method: "get",
         headers: {
           Authorization: `Token ${process.env.HIVE_API_KEY}`,
         },
-      })
-      const json = JSON.parse(JSON.stringify(res))
-      for (const influencer of json.influencers) {
-        const socialAccount = influencer.identity.social_accounts[0].social_account
+      });
+
+      for (const influencer of res.data.influencers) {
+        const socialAccount =
+          influencer.identity.social_accounts[0].social_account;
         lastSocialAccount = {
-          id : socialAccount.id,
+          id: socialAccount.id,
           personal_rank: influencer.personal_rank,
           followers_count: socialAccount.followers_count,
           name: socialAccount.name,
-          screen_name: socialAccount.screen_name
-        }
-        if (lastSocialAccount.followers_count > minimumNbOfFollowers) {
-          yield lastSocialAccount
+          screen_name: socialAccount.screen_name,
+        };
+        if (
+          lastSocialAccount.followers_count >= minimumNbOfFollowers &&
+          lastSocialAccount.personal_rank <= maxQueriedInfluencers
+        ) {
+          readline.cursorTo(process.stdout, 0);
+          process.stdout.write(`downloading ... (${downloadNumber})`);
+          downloadNumber++;
+          yield lastSocialAccount;
         }
       }
-      pageCounter++
-    } while (pageCounter < maxQueriedInfluencers / 50)
+      pageCounter++;
+    } while (pageCounter < maxQueriedInfluencers / 50);
+  }
+
+  public async getTwitterAccountsInCluster(
+    clusterName: string,
+    maxQueriedInfluencers = 10000,
+    defaultValue = 1
+  ): Promise<FetchedData> {
+    const twitterAccounts: FetchedData = {};
+    for await (const account of this.getInfluencersFromClusterWithMinimumFollowers(
+      clusterName,
+      maxQueriedInfluencers
+    )) {
+      twitterAccounts[`twitter:${account.screen_name}:${account.id}`] =
+        defaultValue;
+    }
+    return twitterAccounts;
   }
 
   public async getInfluencersAboveMaxRank(
     influencers: UserData[],
     maxRank: number,
-    clusterNames: string[] = ['Ethereum']
+    clusterNames: string[] = ["Ethereum"]
   ): Promise<string[]> {
     const hiveFetchFunction = async (user: UserData): Promise<string> => {
       try {
