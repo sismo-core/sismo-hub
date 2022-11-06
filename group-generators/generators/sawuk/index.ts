6a6a177c6a219108b873a3d144768363a748b42b
Sismobot <94460392+sismobot@users.noreply.github.com>
2022-11-06 15:27:26 +0100

    [factory]: Adding new sawuk group and its ZK Badge (#297)

---
+++ group-generators/generators/sawuk/index.ts
@@ -0,0 +1,29 @@
+import { Tags, ValueType, GroupWithData, AccountSource } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Once,
+
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    return [
+      {
+        name: "sawuk",
+        timestamp: context.timestamp,
+        data: {
+          "0x6B535aD9c7EEc47c6150c88e9ca70C69bA4D2f60": "1",
+        },
+        accountSources: [AccountSource.ETHEREUM],
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
