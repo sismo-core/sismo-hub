bf59a21922ce15311d84c7802821b21a7ad211bf
bigq <yum0e@protonmail.com>
2022-11-14 16:10:57 +0100

    feat: improve log files

--- src/infrastructure/logger/local-file-logger.ts
+++ src/infrastructure/logger/local-file-logger.ts
@@ -1,5 +1,13 @@
 import fs from "fs";
 import { LoggerService } from "logger/logger";
+
+const logTypeToDecoration: { [logType: string]: string } = {
+  debug: "[DEBUG]: ",
+  info: "[INFO]: ",
+  warning: "[WARNING]: ",
+  error: "[ERROR]: ",
+};
+
 /* istanbul ignore next  */
 export class LocalFileLogger implements LoggerService {
   private _logFilePath: string;
@@ -10,25 +18,33 @@ export class LocalFileLogger implements LoggerService {
 
   debug(...msg: any): void {
     console.debug(...msg);
-    this._write("debug", msg);
+    this._write("debug", msg, "debug");
   }
   info(...msg: any): void {
     console.info(...msg);
-    this._write("info", msg);
+    this._write("info", msg, "info");
+    this._write("debug", msg, "info");
   }
   warning(...msg: any): void {
     console.warn(...msg);
-    this._write("warning", msg);
+    this._write("warning", msg, "warning");
+    this._write("info", msg, "warning");
+    this._write("debug", msg, "warning");
   }
   error(...msg: any): void {
     console.error(...msg);
-    this._write("error", msg);
+    this._write("error", msg, "error");
+    this._write("warning", msg, "error");
+    this._write("info", msg, "error");
+    this._write("debug", msg, "error");
   }
 
-  private _write(logType: string, msg: string[]) {
+  private _write(filename: string, msg: string[], logType: string) {
     fs.appendFileSync(
-      `${this._logFilePath}/${logType}.log`,
-      `${msg.join("\r")}\n`
+      `${this._logFilePath}/${filename}.log`,
+      logType === filename
+        ? `${msg.join("\r")}\n`
+        : `${logTypeToDecoration[logType]}${msg.join("\r")}\n`
     );
   }
 }
