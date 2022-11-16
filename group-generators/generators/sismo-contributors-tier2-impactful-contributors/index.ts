949ee55b3dcfa1a1a7c3bf418f42cc6e6e54daec
MartinGbz <martin.grabarz@outlook.fr>
2022-11-16 19:45:03 +0100

    fix sismo-contributors groups poap provider

--- group-generators/generators/sismo-contributors-tier2-impactful-contributors/index.ts
+++ group-generators/generators/sismo-contributors-tier2-impactful-contributors/index.ts
@@ -27,7 +27,7 @@ const generator: GroupGenerator = {
     groupStore: GroupStore
   ): Promise<GroupWithData[]> => {
     const sismoSubgraphProvider = new dataProviders.SismoSubgraphProvider();
-    const poapProvider = new dataProviders.PoapSubgraphProvider();
+    const poapProvider = new dataProviders.AggregatedPoapSubgraphProvider();
 
     // all minters of this list of ZK badges will be in tier2 in the Sismo Contributors group
     const listOfZkBadgesInTier2 = [
