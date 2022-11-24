e271d8515e3af9650e382e3fe5122571d46afa42
leo Sayous <leo@sismo.io>
2022-11-24 14:31:12 +0100

    fix: remove addresses on the zk hack workshop zk badge

--- group-generators/generators/zk-hack-iii-sismo-workshop/index.ts
+++ group-generators/generators/zk-hack-iii-sismo-workshop/index.ts
@@ -16,10 +16,7 @@ const generator: GroupGenerator = {
         name: "zk-hack-iii-sismo-workshop",
         timestamp: context.timestamp,
         data: {
-          "0xF61CabBa1e6FC166A66bcA0fcaa83762EdB6D4Bd": "1",
           "leo21.eth": "1",
-          "github:leosayous21": "1",
-          "twitter:LeopoldSayous": "1",
         },
         valueType: ValueType.Score,
         tags: [Tags.Factory],
