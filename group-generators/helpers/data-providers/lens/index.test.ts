5d12c060b4dbd87dce06b5fdd18c371ab50ac87a
Benjamin A <97291322+Baoufa@users.noreply.github.com>
2022-12-05 11:04:36 +0100

    fix: adding working test on Lens

--- group-generators/helpers/data-providers/lens/index.test.ts
+++ group-generators/helpers/data-providers/lens/index.test.ts
@@ -1,21 +1,21 @@
-// import {LensProvider} from ".";
+import {LensProvider} from ".";
 
-// describe("Test Lens profile id resolver", () => {
-//   const provider = new LensProvider();
-//   it("should resolve lens profile id from handle", async () => {
-//     const profileId = await provider._getProfileIdFromAnySources("sismo.lens");
-//     expect(profileId).toBe("0x26e5");
-//   });
-//   it("should resolve lens profile id from ethereum account", async () => {
-//     const profileId = await provider._getProfileIdFromAnySources("0xB0A179C459484885D1875009110F3cE3064867B9");
-//     expect(profileId).toBe("0x26e5");
-//   });
-//   it("should resolve lens profile id from ens", async () => {
-//     const profileId = await provider._getProfileIdFromAnySources("vitalik.eth");
-//     expect(profileId).toBe("0x0187b3");
-//   });
-//   it("should resolve lens profile id from profile id", async () => {
-//     const profileId = await provider._getProfileIdFromAnySources("0x26e5");
-//     expect(profileId).toBe("0x26e5");
-//   });
-// });
+describe("Test Lens profile id resolver", () => {
+  const provider = new LensProvider();
+  it("should resolve lens profile id from handle", async () => {
+    const profileId = await provider._getProfileIdFromAnySources("sismo.lens");
+    expect(profileId).toBe("0x26e5");
+  });
+  it("should resolve lens profile id from ethereum account", async () => {
+    const profileId = await provider._getProfileIdFromAnySources("0xB0A179C459484885D1875009110F3cE3064867B9");
+    expect(profileId).toBe("0x26e5");
+  });
+  // it("should resolve lens profile id from ens", async () => {
+  //   const profileId = await provider._getProfileIdFromAnySources("vitalik.eth");
+  //   expect(profileId).toBe("0x0187b3");
+  // });
+  it("should resolve lens profile id from profile id", async () => {
+    const profileId = await provider._getProfileIdFromAnySources("0x26e5");
+    expect(profileId).toBe("0x26e5");
+  });
+});
