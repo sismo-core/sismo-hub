1dea1fb305127aff82cdfde3e6533d74a33db5c3
Sismobot <94460392+sismobot@users.noreply.github.com>
2022-12-23 17:02:31 +0100

    [factory]: Add new zksync-github-stargazers ZK Badge  (#467)

---
+++ group-generators/generators/zksync-github-stargazers/index.ts
@@ -0,0 +1,36 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const githubProvider = new dataProviders.GithubProvider();
+    
+    const githubProviderData0 = await githubProvider.getRepositoriesStargazers({
+      repositories: [ "matter-labs/zksync" ],
+    });
+
+    return [
+      {
+        name: "zksync-github-stargazers",
+        timestamp: context.timestamp,
+        data: githubProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
