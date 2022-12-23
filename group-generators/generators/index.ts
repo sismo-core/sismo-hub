87447a63656bcff6675944f7a1df39d2caf263ce
Sismobot <94460392+sismobot@users.noreply.github.com>
2022-12-23 20:37:06 +0100

    [factory]: Add new zklend-poap-contributor ZK Badge  (#470)

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -124,6 +124,7 @@ import walletGroup from "./wallet-group";
 import wiwNftLegendaryTraders from "./wiw-nft-legendary-traders";
 import zkHack from "./zk-hack";
 import zkHackIiiSismoWorkshop from "./zk-hack-iii-sismo-workshop";
+import zklendPoapContributor from "./zklend-poap-contributor";
 import zksyncGithubStargazers from "./zksync-github-stargazers";
 
 export const groupGenerators: GroupGeneratorsLibrary = {
@@ -251,5 +252,6 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "wiw-nft-legendary-traders": wiwNftLegendaryTraders,
   "zk-hack": zkHack,
   "zk-hack-iii-sismo-workshop": zkHackIiiSismoWorkshop,
+  "zklend-poap-contributor": zklendPoapContributor,
   "zksync-github-stargazers": zksyncGithubStargazers,
 };
---
+++ group-generators/generators/zklend-poap-contributor/index.ts
@@ -0,0 +1,32 @@
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import { GenerationContext, GenerationFrequency, GroupGenerator } from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Once,
+
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    const poapSubgraphProvider = new dataProviders.PoapSubgraphProvider();
+
+    const poapSubgraphProviderData0 = await poapSubgraphProvider.queryEventsTokenOwners({
+      eventIds: [
+        64130, 63629, 48879, 51912, 44747, 47227, 61296, 52411, 52445, 49692, 46534, 62034, 60770,
+        58179, 57129, 54484, 54666, 54885, 53810, 54021, 53655,
+      ],
+    });
+
+    return [
+      {
+        name: "zklend-poap-contributor",
+        timestamp: context.timestamp,
+        data: poapSubgraphProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
