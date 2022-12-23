acf94cf93f29086d902c6ab10fbd175b51ccd828
Sismobot <94460392+sismobot@users.noreply.github.com>
2022-12-23 10:47:21 +0100

    [factory]: Add new empiredao-2023-5-day-desk-pass-zk-badge ZK Badge  (#461)

---
+++ group-generators/generators/empiredao-2023-5-day-desk-pass/index.ts
@@ -0,0 +1,34 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x49B828C60dF0cB4FE8b337AEfb62681365a5B966": "1",
+    };
+
+    return [
+      {
+        name: "empiredao-2023-5-day-desk-pass",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
