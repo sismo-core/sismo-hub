f0b2a8c330d09a8925754aefdf8669f2ace35649
bigq <yum0e@protonmail.com>
2022-12-23 20:33:03 +0100

    fix: proper typo for poap provider

--- group-generators/generators/zklend-poap-contributor/index.ts
+++ group-generators/generators/zklend-poap-contributor/index.ts
@@ -1,24 +1,20 @@
-
 import { dataProviders } from "@group-generators/helpers/data-providers";
 import { Tags, ValueType, GroupWithData } from "topics/group";
-import {
-  GenerationContext,
-  GenerationFrequency,
-  GroupGenerator,
-} from "topics/group-generator";
+import { GenerationContext, GenerationFrequency, GroupGenerator } from "topics/group-generator";
 
 // Generated from factory.sismo.io
 
 const generator: GroupGenerator = {
-  
   generationFrequency: GenerationFrequency.Once,
-  
+
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
-  
     const poapSubgraphProvider = new dataProviders.PoapSubgraphProvider();
-    
+
     const poapSubgraphProviderData0 = await poapSubgraphProvider.queryEventsTokenOwners({
-      eventIds: [ "#64130 #63629 #48879 #51912 #44747 #47227 #61296 #52411 #52445 #49692 #46534 #62034 #60770 #58179 #57129 #54484 #54666 #54885 #53810 #54021 #53655 " ],
+      eventIds: [
+        64130, 63629, 48879, 51912, 44747, 47227, 61296, 52411, 52445, 49692, 46534, 62034, 60770,
+        58179, 57129, 54484, 54666, 54885, 53810, 54021, 53655,
+      ],
     });
 
     return [
