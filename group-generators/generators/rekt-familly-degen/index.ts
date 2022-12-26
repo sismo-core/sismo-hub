83d57cfb58d212e97e1215218843efd0a2e9cac5
Sismobot <94460392+sismobot@users.noreply.github.com>
2022-12-26 16:50:23 +0100

    [factory]: Adding new rekt-familly-degen group and its ZK Badge

---
+++ group-generators/generators/rekt-familly-degen/index.ts
@@ -0,0 +1,36 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x1876A2F935a86E6371E6e56e3Fb3AbcBB50125dA": "1",
+      "0x27D4cfc6DC3a0Ff6a931d19f6d8E743c03bdf88D": "1",
+      "0x550BD1Fe450800A20c8e2c1C514fABb98a7c7739": "1",
+    };
+
+    return [
+      {
+        name: "rekt-familly-degen",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
