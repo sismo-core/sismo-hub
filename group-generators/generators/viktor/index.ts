88e8342db3c62ee80a61e6805b91d18bbd92dc33
Sismobot <94460392+sismobot@users.noreply.github.com>
2022-12-27 19:09:40 +0100

    [factory]: Adding new viktor group and its ZK Badge (#599)

---
+++ group-generators/generators/viktor/index.ts
@@ -0,0 +1,34 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x70828DF613dF170ed2924450ac1107814b31f479": "1",
+    };
+
+    return [
+      {
+        name: "viktor",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
