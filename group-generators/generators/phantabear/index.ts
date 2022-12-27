bf33e8caf336bbb4a1182a56c076edbb02a19ae7
Sismobot <94460392+sismobot@users.noreply.github.com>
2022-12-27 07:39:38 +0100

    [factory]: Adding new phantabear group and its ZK Badge

---
+++ group-generators/generators/phantabear/index.ts
@@ -0,0 +1,34 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x0E386F2AC4ce6cBE7fF14EfCcc04FE41DcA8ea00": "1",
+    };
+
+    return [
+      {
+        name: "phantabear",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
