6fc935961cba44f923ea4b02c861d17b726df586
Sismobot <94460392+sismobot@users.noreply.github.com>
2022-12-27 07:20:18 +0100

    [factory]: Adding new chelsea group and its ZK Badge

--- badges-metadata/playground/factory/hydra-s1-accountbound-factory-badges.ts
+++ badges-metadata/playground/factory/hydra-s1-accountbound-factory-badges.ts
@@ -476,6 +476,31 @@ export const factoryBadges = [
     ],
   },
   {
+    internalCollectionId: 2363873,
+    networks: [
+      Network.Polygon
+    ],
+    name: "chelsea ZK Badge",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n",
+    image: "chelsea.svg",
+    groupGeneratorName: "chelsea",
+    publicContacts: [
+      {
+        type: "twitter",
+        contact: "@kanskey1"
+      },
+      {
+        type: "github",
+        contact: "kanskey"
+      }
+    ],
+    eligibility: {
+      shortDescription: "hold poap",
+      specification: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n"
+    },
+    links: []
+  },
+  {
     internalCollectionId: 2364118,
     networks: [Network.Polygon],
     name: "[playground]  Ens Contracts Stargazers  ZK Badge",
