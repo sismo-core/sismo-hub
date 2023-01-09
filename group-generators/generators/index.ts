f3ba3f3a763418499e3f90621e07765bb677db4e
bigq <99990370+yum0e@users.noreply.github.com>
2023-01-09 19:44:52 +0100

    feat: remove old tutorial (#908)

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -141,7 +141,6 @@ import tokenomicsDaoContributor from "./tokenomics-dao-contributor";
 import top100Ens from "./top-100-ens";
 import tpair from "./tpair";
 import tundraPlayersClub from "./tundra-players-club";
-import tutoEnsContributors from "./tuto-ens-contributors"
 import twitterEthereumInfluencers from "./twitter-ethereum-influencers"
 import unumdao from "./unumdao";
 import viktor from "./viktor";
@@ -298,7 +297,6 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "top-100-ens": top100Ens,
   "tpair": tpair,
   "tundra-players-club": tundraPlayersClub,
-  "tuto-ens-contributors": tutoEnsContributors,
   "twitter-ethereum-influencers": twitterEthereumInfluencers,
   "unumdao": unumdao,
   "viktor": viktor,
