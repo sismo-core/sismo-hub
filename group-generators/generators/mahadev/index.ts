218509acb177bff130c6a6308bcbb62a897279c8
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-09 14:41:28 +0100

    [factory]: Adding new mahadev group and its ZK Badge

---
+++ group-generators/generators/mahadev/index.ts
@@ -0,0 +1,36 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const lensProvider = new dataProviders.LensProvider();
+    
+    const lensProviderData0 = await lensProvider.getFollowers({
+      profileId: "0x1E00E84C6823c6Ce4469B36c082f14Cd9Df5665d"
+    });
+
+    return [
+      {
+        name: "mahadev",
+        timestamp: context.timestamp,
+        data: lensProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
