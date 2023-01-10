6527343f0467e6aad217c5c0d151cbc309e2ac93
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-10 16:14:08 +0100

    [factory]: Adding new cyberconnect group and its ZK Badge

---
+++ group-generators/generators/cyberconnect/index.ts
@@ -0,0 +1,34 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Weekly,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x7927f89670F942169476b006966dd6ECCdc7425c": "1",
+    };
+
+    return [
+      {
+        name: "cyberconnect",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
