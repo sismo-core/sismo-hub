181f20c39f65e347f9c04e3503929370fe67673d
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-11 19:32:07 +0100

    [factory]: Adding new roxxypoxxy group and its ZK Badge (#967)

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -121,6 +121,7 @@ import rektFamillyDegen from "./rekt-familly-degen";
 import relayBadge from "./relay-badge";
 import rhinofiPowerUsers from "./rhinofi-power-users";  
 import rocketlab from "./rocketlab";
+import roxxypoxxy from "./roxxypoxxy";
 import sanyiSGift from "./sanyi-s-gift";
 import sardineEnthusiasts from "./sardine-enthusiasts";
 import sawuk from "./sawuk";
@@ -244,6 +245,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "rahulkr-lens-followers": rahulkrLensFollowers,
   "ravi-zk-badge-on-sismo": raviZkBadgeOnSismo,
   "rekt-familly-degen": rektFamillyDegen,
+  "roxxypoxxy": roxxypoxxy,
   "sanyi-s-gift": sanyiSGift,
   "sismo-meme": sismoMeme,
   "sismo-oarctic": sismoOarctic,
