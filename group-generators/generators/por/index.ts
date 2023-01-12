734d05291faab3c5f4e1359f791555a042abb428
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-12 12:09:30 +0100

    [factory]: Adding new por group and its ZK Badge

---
+++ group-generators/generators/por/index.ts
@@ -0,0 +1,34 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0xfDC46fBDd8AF50d9Bf7536Bf44ce8560E423352c": "1",
+    };
+
+    return [
+      {
+        name: "por",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
