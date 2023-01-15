d222dd02d7b7b6c1ff6b636b7e00be5bf3516f8a
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-15 15:30:26 +0100

    [factory]: Adding new alpha-drops group and its ZK Badge (#1022)

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -3,6 +3,7 @@ import { GroupGeneratorsLibrary } from "topics/group-generator";
 import akamo from "./akamo";
 import akapsFrens from "./akaps-frens";
 import all4you4 from "./all4you4";
+import alphaDrops from "./alpha-drops";
 import ambireFrens from "./ambire-frens";
 import anavSDemo from "./anav-s-demo";
 import anavSTrial from "./anav-s-trial";
@@ -226,6 +227,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "akamo": akamo,
   "akaps-frens": akapsFrens,
   "all4you4": all4you4,
+  "alpha-drops": alphaDrops,
   "ambire-frens": ambireFrens,
   "anav-s-demo": anavSDemo,
   "anav-s-trial": anavSTrial,
