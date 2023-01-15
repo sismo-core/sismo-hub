dc924fb865ded5eea01b2dc951409c1f42f5e4c2
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-15 22:21:35 +0100

    [factory]: Adding new roadtolife group and its ZK Badge (#1052)

---
+++ group-generators/generators/roadtolife/index.ts
@@ -0,0 +1,34 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0xf48350c2c3B39A84b919dD88c29E1B3ab24841D8": "1",
+    };
+
+    return [
+      {
+        name: "roadtolife",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
