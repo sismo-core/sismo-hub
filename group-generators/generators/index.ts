c9c3df652b0773de6cd2eede7f9694c568495597
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-16 06:53:54 +0100

    [factory]: Adding new gem group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -68,6 +68,7 @@ import flovatar from "./flovatar";
 import foxfamAdorator from "./foxfam-adorator";
 import friendOf0xba3299d from "./friend-of-0xba3-299d";
 import gamejustuAchievements from "./gamejutsu-achievements";
+import gem from "./gem";
 import gen0Dagorians from "./gen-0-dagorians";
 import gitcoinGrantsAggregatedRoundsDonors from "./gitcoin-grants-aggregated-rounds-donors";
 import gitcoinGrantsRoundsApiDonors from "./gitcoin-grants-rounds-api-donors";
@@ -288,6 +289,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "flovatar": flovatar,
   "foxfam-adorator": foxfamAdorator,
   "friend-of-0xba3-299d": friendOf0xba3299d,
+  "gem": gem,
   "gen-0-dagorians": gen0Dagorians,
   "gotchi-french-army-x-sismo-live": gotchiFrenchArmyXSismoLive,
   "gugulan": gugulan,
