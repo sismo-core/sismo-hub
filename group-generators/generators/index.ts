68f17f09d734ab35e1380f49c05d442ff6274684
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-18 09:39:21 +0100

    [factory]: Adding new mikedoge group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -127,6 +127,7 @@ import masqueradePolygonZkBadgeHolders from "./masquerade-polygon-zk-badge-holde
 import mathcastles from "./mathcastles";
 import mcgregor from "./mcgregor";
 import miceDerpEarly from "./mice-derp-early";
+import mikedoge from "./mikedoge";
 import mintanklee from "./mintanklee";
 import mmmemelord from "./mmmemelord";
 import monsters from "./monsters";
@@ -341,6 +342,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "lens-meme-collector-mirror": lensMemeCollectorMirror,
   "mahadev": mahadev,
   "lenster-gas-supporter": lensterGasSupporter,
+  "mikedoge": mikedoge,
   "mmmemelord": mmmemelord,
   "moon-dao": moonDao,
   "my-1": my1,
