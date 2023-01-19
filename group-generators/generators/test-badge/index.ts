a60f501a48145d1773b74f55cf04458f1c66bc25
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-19 06:00:16 +0100

    [factory]: Adding new test-badge group and its ZK Badge

---
+++ group-generators/generators/test-badge/index.ts
@@ -0,0 +1,35 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0xB8C3CC00Fdd5C24dFeb9c5609203A0D434283c9C": "1",
+      "0xC536977f5207eCe8ff654F92861426cD27c5aB5B": "1",
+    };
+
+    return [
+      {
+        name: "test-badge",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
