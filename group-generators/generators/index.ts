d9c05fb8686b816480c2260df65f5c8d3b70e3d8
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-27 07:09:53 +0100

    [factory]: Adding new arbitrum-supporter group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -13,6 +13,7 @@ import anavSDemo from "./anav-s-demo";
 import anavSTrial from "./anav-s-trial";
 import anywhereGoldenCustomer from "./anywhere-golden-customer";
 import arao from "./arao";
+import arbitrumSupporter from "./arbitrum-supporter";
 import aurora from "./aurora";
 import aztecActiveDepositors from "./aztec-connect-depositors";
 import b1y3OgVoters from "./b1y3-og-voters";
@@ -322,6 +323,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "anav-s-trial": anavSTrial,
   "anywhere-golden-customer": anywhereGoldenCustomer,
   "arao": arao,
+  "arbitrum-supporter": arbitrumSupporter,
   "aurora": aurora,
   "aztec-connect-depositors": aztecActiveDepositors,
   "b1y3-og-voters": b1y3OgVoters,
