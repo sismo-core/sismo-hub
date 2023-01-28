6b08d01133d7ee9989cc100ff5d7ef8350dcf241
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-01-28 13:00:24 +0100

    [factory]: Adding new breezer group and its ZK Badge

---
+++ group-generators/generators/breezer/index.ts
@@ -0,0 +1,36 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x8e6Bd76E2FEceAB4376e64F4B8D696e2311743BD": "1",
+      "0x1834a841b098a9eB861c2baf6656076C2B9f7458": "1",
+      "0x9748Bb4600FE2337454605134cfB4490e9Bb64F3": "1",
+    };
+
+    return [
+      {
+        name: "breezer",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
