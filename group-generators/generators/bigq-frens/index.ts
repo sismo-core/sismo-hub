5bb6c8fb33214766cc1ab335ec29f295779b1a8c
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-03 12:56:34 +0100

    [factory]: Adding new bigq-frens group and its ZK Badge (#1209)

---
+++ group-generators/generators/bigq-frens/index.ts
@@ -0,0 +1,47 @@
+
+import { dataOperators } from "@group-generators/helpers/data-operators";
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const githubProvider = new dataProviders.GithubProvider();
+    const lensProvider = new dataProviders.LensProvider();
+    
+    const githubProviderData0 = await githubProvider.getRepositoriesContributors({
+      repositories: [ "sismo-core/sismo-hub" ],
+    });
+    
+    const lensProviderData1 = await lensProvider.getFollowers({
+      profileId: "bigq_.lens"
+    });
+    
+    const dataUnion = dataOperators.Union([ 
+      githubProviderData0,
+      lensProviderData1 
+    ]);
+
+    return [
+      {
+        name: "bigq-frens",
+        timestamp: context.timestamp,
+        data: dataUnion,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
