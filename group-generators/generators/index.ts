1e3a75e650ae18f4f7af73dbd3755e21ea0c1d8b
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-03 03:06:09 +0100

    [factory]: Adding new phuln13 group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -199,6 +199,7 @@ import oxbage from "./oxbage";
 import padultra from "./padultra";
 import petrolistul from "./petrolistul";
 import phantabear from "./phantabear";
+import phuln13 from "./phuln13";
 import pohPolygonZkBadgeHolders from "./poh-polygon-zk-badge-holders";
 import polyxExchange from "./polyx-exchange";
 import por from "./por";
@@ -527,6 +528,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "padultra": padultra,
   "petrolistul": petrolistul,
   "phantabear": phantabear,
+  "phuln13": phuln13,
   "poh-polygon-zk-badge-holders": pohPolygonZkBadgeHolders,
   "polyx-exchange": polyxExchange,
   "por": por,
