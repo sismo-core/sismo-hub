244223191e7fc418120d2101a523b3ede31949de
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-12 19:20:46 +0100

    [factory]: Adding new troix123 group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -310,6 +310,7 @@ import tokenomicsDaoConsultant from "./tokenomics-dao-consultant";
 import tokenomicsDaoContributor from "./tokenomics-dao-contributor";
 import top100Ens from "./top-100-ens";
 import tpair from "./tpair";
+import troix123 from "./troix123";
 import tundraPlayersClub from "./tundra-players-club";
 import tutoEnsContributors from "./tuto-ens-contributors"
 import twitter from "./twitter";
@@ -664,6 +665,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "tokenomics-dao-contributor": tokenomicsDaoContributor,
   "top-100-ens": top100Ens,
   "tpair": tpair,
+  "troix123": troix123,
   "tundra-players-club": tundraPlayersClub,
   "tuto-ens-contributors": tutoEnsContributors,
   "twitter": twitter,
