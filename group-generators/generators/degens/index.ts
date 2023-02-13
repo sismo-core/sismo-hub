4a16c610d5fded5f2cc99eddb8882b2287c1e279
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-13 14:32:25 +0100

    [factory]: Adding new degens group and its ZK Badge

---
+++ group-generators/generators/degens/index.ts
@@ -0,0 +1,36 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const poapSubgraphProvider = new dataProviders.PoapSubgraphProvider();
+    
+    const poapSubgraphProviderData0 = await poapSubgraphProvider.queryEventsTokenOwners({
+      eventIds: [ "2435941", "28101", "19094", "23333", "48890", "197537", "3136", "37847", "100504" ],
+    });
+
+    return [
+      {
+        name: "degens",
+        timestamp: context.timestamp,
+        data: poapSubgraphProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
