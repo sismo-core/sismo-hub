1a8e5eb9f9664c2e55c1edb8fda1c65f33246761
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-15 15:56:24 +0100

    [factory]: Adding new lazybaer group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -158,6 +158,7 @@ import LaMeute from "./la-meute";
 import lamaPama from "./lama-pama";
 import lands from "./lands";
 import lasttrade161 from "./lasttrade161";
+import lazybaer from "./lazybaer";
 import leidream from "./leidream";
 import lens50BestFollowed from "./lens-50-best-followed";
 import lensFollowers from "./lens-followers";
@@ -530,6 +531,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "lama-pama": lamaPama,
   "lands": lands,
   "lasttrade161": lasttrade161,
+  "lazybaer": lazybaer,
   "leidream": leidream,
   "lens-50-best-followed": lens50BestFollowed,
   "lens-followers": lensFollowers,
---
+++ group-generators/generators/lazybaer/index.ts
@@ -0,0 +1,44 @@
+
+import { dataOperators } from "@group-generators/helpers/data-operators";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Daily,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x60d3130a2F3f8C04ee54D3cF3863e7aDd8ebA322": "1",
+    };
+    
+    const jsonListData1 = {
+      "0xC96DEdb0F0972b90715FD58c75012Ca995Cb3F8E": "1",
+    };
+    
+    const dataUnion = dataOperators.Union([ 
+      jsonListData0,
+      jsonListData1 
+    ]);
+
+    return [
+      {
+        name: "lazybaer",
+        timestamp: context.timestamp,
+        data: dataUnion,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
