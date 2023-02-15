7ab441c2bf2c9a4be7dbccce9c3a627d69b91af5
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-15 13:54:20 +0100

    [factory]: Adding new ai-humanode group and its ZK Badge (#1264)

---
+++ group-generators/generators/ai-humanode/index.ts
@@ -0,0 +1,36 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const poapSubgraphProvider = new dataProviders.PoapSubgraphProvider();
+    
+    const poapSubgraphProviderData0 = await poapSubgraphProvider.queryEventsTokenOwners({
+      eventIds: [ "33142" ],
+    });
+
+    return [
+      {
+        name: "ai-humanode",
+        timestamp: context.timestamp,
+        data: poapSubgraphProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
