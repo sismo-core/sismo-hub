df658a67dff947bede0ebd02b07267d2cad113a7
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-16 22:44:16 +0100

    [factory]: Adding new love group and its ZK Badge (#1281)

---
+++ group-generators/generators/love/index.ts
@@ -0,0 +1,41 @@
+
+import { dataOperators } from "@group-generators/helpers/data-operators";
+import { Tags, ValueType, GroupWithData, GroupStore } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  dependsOn: ["turkiye-earthquake-relief-dao"],
+  
+  generate: async (context: GenerationContext, groupStore: GroupStore): Promise<GroupWithData[]> => {
+  
+    
+    const turkiyeEarthquakeReliefDaoGroupLatest = await groupStore.latest(
+      "turkiye-earthquake-relief-dao"
+    );
+    
+    const turkiyeEarthquakeReliefDaoData0 = dataOperators.Map(
+      await turkiyeEarthquakeReliefDaoGroupLatest.data(),
+      1
+    );
+
+    return [
+      {
+        name: "love",
+        timestamp: context.timestamp,
+        data: turkiyeEarthquakeReliefDaoData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
