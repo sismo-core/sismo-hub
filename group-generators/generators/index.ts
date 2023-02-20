6671d3d93bf7f941211264e607f4712c1b4a1f84
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-20 08:34:38 +0100

    [factory]: Adding new trove group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -350,6 +350,7 @@ import tokenomicsDaoContributor from "./tokenomics-dao-contributor";
 import top100Ens from "./top-100-ens";
 import tpair from "./tpair";
 import troix123 from "./troix123";
+import trove from "./trove";
 import tundraPlayersClub from "./tundra-players-club";
 import turkiyeEarthquakeReliefDao from "./turkiye-earthquake-relief-dao";
 import tutoEnsContributors from "./tuto-ens-contributors"
@@ -747,6 +748,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "top-100-ens": top100Ens,
   "tpair": tpair,
   "troix123": troix123,
+  "trove": trove,
   "tundra-players-club": tundraPlayersClub,
   "turkiye-earthquake-relief-dao": turkiyeEarthquakeReliefDao,
   "tuto-ens-contributors": tutoEnsContributors,
