8ba55cfffb8fd402c8cb4899697e9216dd4d0209
MartinGbz <martin.grabarz@outlook.fr>
2023-02-20 20:07:09 +0100

    fix: clean code + add retry request

--- group-generators/helpers/data-providers/farcaster/index.ts
+++ group-generators/helpers/data-providers/farcaster/index.ts
@@ -1,142 +1,100 @@
 import readline from "readline";
 import { RestProvider } from "@group-generators/helpers/data-providers/rest-api";
 import { FetchedData } from "topics/group";
+import console from "console";
 
 export class FarcasterProvider {
-    restProvider: RestProvider;
-    url: string;
-    headers: {
-        "authorization": string;
-        "accept": string;
-      };
+  restProvider: RestProvider;
+  url: string;
+  headers: {
+    authorization: string;
+    accept: string;
+  };
 
-    public constructor() {
-        this.restProvider = new RestProvider();
-        this.url = "https://api.farcaster.xyz/v2/";
-        this.headers = {
-            "authorization": process.env.FARCASTER_API_KEY as string,
-            "accept": 'application/json'
+  public constructor() {
+    this.restProvider = new RestProvider();
+    this.url = "https://api.farcaster.xyz/v2/";
+    this.headers = {
+      authorization: process.env.FARCASTER_API_KEY as string,
+      accept: "application/json",
+    };
+  }
+
+
+  public async getAllUsers(): Promise<FetchedData> {
+    const dataProfiles: FetchedData = {};
+    const numberOfUsers = await this.getLastCreatedFid();
+    let profileChunks: Promise<string>[] = [];
+    const chunks = 100;
+    const chunksWaitTime = 10000;
+    const chunksWaitRetry = 10000;
+
+    const retryRequest = async (fid: number, numberOfRetry=5) => {
+      let error;
+      for (let i = 0; i < numberOfRetry; i++) {
+        try {
+          return await this.resolveAddress(fid);
+        } catch (err) {
+          // console.log("---------------res");
+          // console.log(err);
+          await new Promise((resolve: any) => setTimeout(resolve, chunksWaitRetry))
         }
+      }
+      throw new Error('Max retry reached+\n' + error);
     }
 
+    for (let i = 1; i <= numberOfUsers; i++) {
+      
+      profileChunks.push(retryRequest(i));
 
-    public async getAllUsers(): Promise<FetchedData> {
-        const dataProfiles: FetchedData = {};
-        const numberOfUsers = await this.getLastCreatedFidQuery();
-        // const numberOfUsers = 500;
-        console.log(`Number of users: ${numberOfUsers}`);
-        let profileChunks: Promise<string>[] = [];
-        
-        // start at 2 because fid 1 return an empty verifications array
-        for(let i = 2; i <= numberOfUsers; i++) {
-            profileChunks.push(this.resolveAddress(i));
-            // console.log('i');
-            // console.log(i);
-            // console.log('profileChunks.length');
-            // console.log(profileChunks.length);
-            if(profileChunks.length % 100 == 0 || i == numberOfUsers) {
-
-                // const profileChunksPromise = profileChunks.map(fid => this.resolveAddress(fid));
-                await Promise.all(profileChunks).then((addresses) => {
-                    // console.log('profileChunks');
-                    // console.log(profileChunks);
-                    // console.log(addresses);
-                    
-                    addresses.forEach((address) => {
-                        if(address != ''){
-                            dataProfiles[address] = 1;
-                        }
-                    });
-                });
-                readline.cursorTo(process.stdout, 0);
-                process.stdout.write(`Profiles fetched: ${Object.keys(dataProfiles).length}`);
-                profileChunks = [];
-                await new Promise((resolve: any) => setTimeout(resolve, 10000))
+      if (profileChunks.length % chunks == 0 || i == numberOfUsers) {
+        await Promise.all(profileChunks)
+        .then((addresses) => {
+          addresses.forEach((address) => {
+            if (address != "") {
+              dataProfiles[address] = 1;
             }
-        }
+          });
+        })
+        .catch(error => {throw new Error(error)})
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write(
+          `Profiles fetched: ${Object.keys(dataProfiles).length}`
+        );
+        profileChunks = [];
+        await new Promise((resolve: any) => setTimeout(resolve, chunksWaitTime));
+      }
 
-        return dataProfiles;
     }
+    return dataProfiles;
+  }
 
-    public async getLastCreatedFidQuery(): Promise<number> {
-        const res = await this.restProvider.fetchData({
-            url: this.url+"recent-users",
-            method: "get",
-            headers: this.headers,
-        });
+  public async getLastCreatedFid(): Promise<number> {
+    const res = await this.restProvider.fetchData({
+      url: this.url + "recent-users",
+      method: "get",
+      headers: this.headers,
+    });
 
-        if(Object(res).result) {
-            // success
-            return Object(res).result.users[0].fid;
-        }
-        else {
-            throw new Error(Object(res));
-        }
+    if (Object(res).result) {
+      // success
+      return Object(res).result.users[0].fid;
+    } else {
+      throw new Error(Object(res));
     }
+  }
 
-    public async resolveAddress(fid: number): Promise<string> {
-        const res = await this.restProvider.fetchData({
-            url: this.url+"verifications?fid="+fid,
-            method: "get",
-            headers: this.headers,
-        });
+  public async resolveAddress(fid: number): Promise<string> {
+    const res = await this.restProvider.fetchData({
+      url: this.url + "verifications?fid=" + fid,
+      method: "get",
+      headers: this.headers,
+    });
 
-        // console.log(res);
-        // console.log(Object(res).result.verifications.length)
-
-        if(Object(res).result.verifications.length > 0) {
-            // success
-            return Object(res).result.verifications[0].address;
-        }
-        else {
-            // console.log('Verifications array is empty');
-            // console.log(fid);
-            return '';
-            // console.log();
-        }
+    if (Object(res).result.verifications.length > 0) {
+      return Object(res).result.verifications[0].address;
+    } else {
+      return "";
     }
-
-    // public async getRecentUsers(limit = 1000, cursor?: string): Promise<FetchedData> {
-    //     let continueFetch = true;
-    //     let data: FetchedData;
-    //     while(continueFetch) {
-    //         const res = await this.getRecentUserQuery(limit, cursor);
-            
-    //         if(Object(res).next) {
-    //             cursor = Object(res).next;
-    //         }
-    //         else {
-    //             continueFetch = false;
-    //         }
-    //     }
-    // }
-
-    // public async getRecentUserQuery(limit?: number, cursor?: string): Promise<RecentUsersSuccessResponse|RecentUsersErrorResponse> {
-    //     const res = await this.restProvider.fetchData({
-    //         url: this.url+"recent-users",
-    //         method: "get",
-    //         headers: this.headers,
-    //         data: {
-    //             limit: limit,
-    //             cursor: cursor
-    //         }
-    //     });
-
-    //     if(Object(res).result) {
-    //         // success
-    //         return {
-    //             results: Object(res).results,
-    //             next: Object(res).next
-    //         } as RecentUsersSuccessResponse;
-    //     }
-    //     else {
-    //         // error
-    //         const response: RecentUsersErrorResponse = {
-    //             errors: Object(res).errors,
-    //         }
-    //         console.error(response);
-    //         return response;
-    //     }
-    // }
-
+  }
 }
