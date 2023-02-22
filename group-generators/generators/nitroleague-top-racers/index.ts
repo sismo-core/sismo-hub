3b17f7b91175215c090a38be8280af8927aac7d2
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-22 06:46:31 +0100

    [factory]: Adding new nitroleague-top-racers group and its ZK Badge

---
+++ group-generators/generators/nitroleague-top-racers/index.ts
@@ -0,0 +1,34 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x30400435Dd2b6E41e2E0a6583093d947fa731331": "1",
+    };
+
+    return [
+      {
+        name: "nitroleague-top-racers",
+        timestamp: context.timestamp,
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
