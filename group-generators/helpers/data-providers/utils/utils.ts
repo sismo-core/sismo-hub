b5e28c93453bcd6905e5bf4a3cdb9c5c0291089c
MartinGbz <martin.grabarz@outlook.fr>
2023-02-23 17:24:16 +0100

    fix: retry for any types of error

--- group-generators/helpers/data-providers/utils/utils.ts
+++ group-generators/helpers/data-providers/utils/utils.ts
@@ -10,14 +10,8 @@ export const retryRequest = async (context: any, request: any, cursor: any, retr
     try {
       return await request(context, cursor);
     } catch (err: any) {
-      if (err.response.status == 429 || err.response.status == -1) {
-        await new Promise((resolve: any) =>
-          setTimeout(resolve, 15000)
-        );
-      }
-      else {
-        throw new Error(error);
-      }
+      await new Promise((resolve: any) =>
+      setTimeout(resolve, 15000));
     }
   }
   throw new Error("Max retry reached\n" + error);
