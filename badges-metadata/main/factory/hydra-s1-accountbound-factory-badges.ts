1deb4dd86ade87df1e87105bade922f1a1d92d0c
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-23 09:32:27 +0100

    [factory]: Add new treasure ZK Badge  (#1335)

--- badges-metadata/main/factory/hydra-s1-accountbound-factory-badges.ts
+++ badges-metadata/main/factory/hydra-s1-accountbound-factory-badges.ts
@@ -7332,6 +7332,23 @@ export const factoryBadges: BadgeMetadata[] = [
     links: [],
   },
   {
+    internalCollectionId: 2985570,
+    networks: [
+      Network.Polygon
+    ],
+    name: "Treasure",
+    description: "Badge available for voting in Treasure Dao on Snapshot",
+    image: "treasure.svg",
+    groupSnapshot: { groupName: "treasure" },
+    publicContacts: [
+      {
+        type: "twitter",
+        contact: "@buragan1978"
+      }
+    ],
+    links: []
+  },
+  {
     internalCollectionId: 2986697,
     networks: [
       Network.Polygon
