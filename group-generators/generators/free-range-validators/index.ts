4e827db2115991e99a637f7c2fb795fd1a6ce996
Y6NDR <19380973+thebeyondr@users.noreply.github.com>
2023-02-26 14:24:17 -0700

    feat: :sparkles: Add `free-range-validators` group generator & badge (#1351)

---
+++ group-generators/generators/free-range-validators/index.ts
@@ -0,0 +1,43 @@
+// import { dataProviders } from '@group-generators/helpers/data-providers';
+import { dataOperators } from "@group-generators/helpers/data-operators";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Once,
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    // const restProvider = new dataProviders.RestProvider();
+    // const validators = await restProvider.getAccountsFromAPI({
+    //     url: 'http://freerangedao.xyz/api/allowlist',
+    //     method: 'GET',
+    // });
+
+    const testAddresses = {
+      "0xc4EE7d84038A3f09237AfF8B59da65D1F22351ff": 32000000000,
+      "0x28EDE9352a5f76Daec81cfC65d7246f6665f5fA3": 32000000000,
+      "leo21.sismo.eth": 32000000000,
+      "0x23314160c752D6Bb544661DcE13d01C21c64331E": 32000000000,
+    };
+
+    // Map to have only 1 as value
+    const data = dataOperators.Map(testAddresses, 1);
+
+    return [
+      {
+        name: "free-range-validators",
+        specs: "Validators on our allowlist",
+        description: "Validators eligible to take the Free Range Survey",
+        timestamp: context.timestamp,
+        data,
+        valueType: ValueType.Score,
+        tags: [Tags.User],
+      },
+    ];
+  },
+};
+
+export default generator;
