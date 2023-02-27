12b49cf671ef95cd2f638e57971503a7ecba86f6
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-02-27 14:41:58 +0100

    [factory]: Adding new mbank-builders group and its ZK Badge (#1381)

---
+++ group-generators/generators/mbank-builders/index.ts
@@ -0,0 +1,36 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Weekly,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0xd522B0aEb1367579F6d2ef2166d77736cDc1Ed3B": "1",
+    };
+
+    return [
+      {
+        name: "mbank-builders",
+        timestamp: context.timestamp,
+        description: "Community of Bank Of Georgia mBank builders",
+        specs: "Contributor of mBank",
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
