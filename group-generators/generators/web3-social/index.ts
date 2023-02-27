83a732648919813736a738edbd36e17980dfdafe
MartinGbz <martin.grabarz@outlook.fr>
2023-02-27 16:50:51 +0100

    feat: add web3-social group

---
+++ group-generators/generators/web3-social/index.ts
@@ -0,0 +1,36 @@
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import {
+  ValueType,
+  Tags,
+  FetchedData,
+  GroupWithData,
+  AccountSource,
+} from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Daily,
+
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    const lensProvider = new dataProviders.LensProvider();
+
+    const dataProfiles: FetchedData = await lensProvider.getProfiles();
+
+    return [
+      {
+        name: "web3-social",
+        timestamp: context.timestamp,
+        data: dataProfiles,
+        accountSources: [AccountSource.ETHEREUM],
+        valueType: ValueType.Info,
+        tags: [Tags.User, Tags.Lens, Tags.Web3Social],
+      },
+    ];
+  },
+};
+
+export default generator;
