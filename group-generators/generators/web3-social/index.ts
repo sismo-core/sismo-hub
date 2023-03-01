82f9cb5e2fd46cfe69c8af1eca949f42a0dd2028
MartinGbz <martin.grabarz@outlook.fr>
2023-03-01 10:31:50 +0100

    disable web3-social group (#1405)

--- group-generators/generators/web3-social/index.ts
+++ group-generators/generators/web3-social/index.ts
@@ -1,5 +1,5 @@
-import { dataOperators } from "@group-generators/helpers/data-operators";
-import { dataProviders } from "@group-generators/helpers/data-providers";
+// import { dataOperators } from "@group-generators/helpers/data-operators";
+// import { dataProviders } from "@group-generators/helpers/data-providers";
 import { ValueType, Tags, GroupWithData } from "topics/group";
 import {
   GenerationContext,
@@ -11,18 +11,18 @@ const generator: GroupGenerator = {
   generationFrequency: GenerationFrequency.Once,
 
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
-    const farcasterProvider = new dataProviders.FarcasterProvider();
-    const lensProvider = new dataProviders.LensProvider();
+    // const farcasterProvider = new dataProviders.FarcasterProvider();
+    // const lensProvider = new dataProviders.LensProvider();
 
-    const [farcasterProfiles, lensProfiles] = await Promise.all([
-      farcasterProvider.getAllUsers(),
-      lensProvider.getAllProfiles(),
-    ]);
+    // const [farcasterProfiles, lensProfiles] = await Promise.all([
+    //   farcasterProvider.getAllUsers(),
+    //   lensProvider.getAllProfiles(),
+    // ]);
 
-    const web3SocialProfiles = dataOperators.Union([
-      lensProfiles,
-      farcasterProfiles,
-    ]);
+    // const web3SocialProfiles = dataOperators.Union([
+    //   lensProfiles,
+    //   farcasterProfiles,
+    // ]);
 
     return [
       {
@@ -30,7 +30,7 @@ const generator: GroupGenerator = {
         timestamp: context.timestamp,
         description: "ZK Badge owned by all web3 social users",
         specs: "Have a profile on Lens or Farcaster",
-        data: web3SocialProfiles,
+        data: {},
         valueType: ValueType.Info,
         tags: [Tags.User, Tags.Lens, Tags.Web3Social],
       },
