5b3c96b93e4368fb9e29b29483cce32532262854
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-03-03 02:42:01 +0100

    [factory]: Adding new siddxa-lens-follow group and its ZK Badge

---
+++ group-generators/generators/siddxa-lens-follow/index.ts
@@ -0,0 +1,48 @@
+
+import { dataOperators } from "@group-generators/helpers/data-operators";
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const lensProvider = new dataProviders.LensProvider();
+    
+    const lensProviderData0 = await lensProvider.getFollowers({
+      profileId: "siddxa.lens"
+    });
+    
+    const lensProviderData1 = await lensProvider.getFollowers({
+      profileId: "siddxa.eth"
+    });
+    
+    const dataUnion = dataOperators.Union([ 
+      lensProviderData0,
+      lensProviderData1 
+    ]);
+
+    return [
+      {
+        name: "siddxa-lens-follow",
+        timestamp: context.timestamp,
+        description: "Lens followers of siddxa.lens",
+        specs: "Lens followers of siddxa.lens",
+        data: dataUnion,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
