f03c7d852dbd5527eb04d5668fbfb318b9edb3ea
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-03-17 17:21:39 +0100

    [factory]: Adding new one-5-stars-job-completed-for-aave-on-talentlayer-protocol group and its ZK Badge

---
+++ group-generators/generators/one-5-stars-job-completed-for-aave-on-talentlayer-protocol/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Daily,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const talentLayerProvider = new dataProviders.TalentLayerProvider();
+    
+    const talentLayerProviderData0 = await talentLayerProvider.didSellerServiceBuyer({
+      userHandle: "alice"
+    });
+
+    return [
+      {
+        name: "one-5-stars-job-completed-for-aave-on-talentlayer-protocol",
+        timestamp: context.timestamp,
+        description: "Complete work for AAVE with minimum 5 as rating",
+        specs: "Collect all users that completed work at least 1 times with a 5 rating on TalentLayer protocol",
+        data: talentLayerProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
