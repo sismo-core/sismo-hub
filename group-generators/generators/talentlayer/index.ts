d759d47114e30ce2f8975bcd5baa12f361e8cc92
Romain <romain@talentlayer.org>
2023-03-17 14:50:29 +0100

    Zkpow (#1532)
    * add talentlayer data provider
    
    * fix: added count structure with not optimal query -> count query should be provided on The Graph for increased performance
    
    * renaming functions
    
    * fix: add count function to dataprovider index
    
    * update data structure
    
    * remove argument
    
    * update response format
    
    * Add new users group for talentLayer
    
    * feat: added DidSellerWorkForBuyerGroup data
    
    * Add didWorkOnTopic + Adjust typing
    
    * feat: getUserTotalSalary
    
    * generateTopicGroup solidity
    
    * Import talentlayer schema
    
    * configure function args
    
    * fix: didSellerServiceBuyer
    
    * update count logical
    
    * fix query and create query for reviews
    
    * add review typing
    
    * fix services query
    
    * fix generate
    
    * fix generate
    
    * fix: getUserTotalEarned
    
    * rating group + handle count
    
    * fix: added function to schema
    
    * handle count for didUserMinimalEarnedOfToken
    
    * fix: added didSellerServiceBuyer to schema
    
    * Auto review
    
    ---------
    
    Co-authored-by: Daniel de Witte <Artimunor@users.noreply.github.com>

--- group-generators/generators/talentlayer/index.ts
+++ group-generators/generators/talentlayer/index.ts
@@ -49,14 +49,132 @@ const generateUsersGroup = async (
   };
 };
 
+const generateTopicGroup = async (
+  context: GenerationContext,
+  topic: string,
+  numberOfTimes: number
+): Promise<GroupWithData> => {
+  const talentLayerProvider = new dataProviders.TalentLayerProvider();
+
+  const didWork = await talentLayerProvider.didWorkOnTopic(
+    topic,
+    numberOfTimes
+  );
+
+  return {
+    name: `talentlayer-${topic}-${numberOfTimes}`,
+    timestamp: context.timestamp,
+    description: `Be skilled in ${topic}`,
+    specs: `Collect all users that already work at least ${numberOfTimes} times on a ${topic} project on TalentLayer protocol`,
+    data: didWork,
+    valueType: ValueType.Score,
+    tags: [Tags.User],
+  };
+};
+
+const generateRatingGroup = async (
+  context: GenerationContext,
+  minRating: number,
+  numberOfTimes: number
+): Promise<GroupWithData> => {
+  const talentLayerProvider = new dataProviders.TalentLayerProvider();
+
+  const didWorkWithRating = await talentLayerProvider.didWorkWithRating(
+    minRating,
+    numberOfTimes
+  );
+
+  return {
+    name: `talentlayer-rating${minRating}-${numberOfTimes}`,
+    timestamp: context.timestamp,
+    description: `Complete work with minimum ${minRating} as rating`,
+    specs: `Collect all users that completed work at least ${numberOfTimes} times with a ${minRating} rating on TalentLayer protocol`,
+    data: didWorkWithRating,
+    valueType: ValueType.Score,
+    tags: [Tags.User],
+  };
+};
+
+const generateDidSellerServiceForBuyerGroup = async (
+  context: GenerationContext,
+  buyer: string,
+  minimalServices: number
+): Promise<GroupWithData> => {
+  const talentLayerProvider = new dataProviders.TalentLayerProvider();
+
+  const didWork = await talentLayerProvider.didSellerServiceBuyer(
+    buyer,
+    minimalServices
+  );
+
+  return {
+    name: "talentlayer-did-work-for",
+    timestamp: context.timestamp,
+    description: "Find out if a user did work for a company",
+    specs:
+      "Check to see if a user did work for a company by checking the subgraph",
+    data: didWork,
+    valueType: ValueType.Score,
+    tags: [Tags.User],
+  };
+};
+
+const generateDidUserMinimalEarnedOfTokenGroup = async (
+  context: GenerationContext,
+  userAddress: string,
+  minimalEarned: number,
+  tokenSymbol: string
+): Promise<GroupWithData> => {
+  const talentLayerProvider = new dataProviders.TalentLayerProvider();
+
+  const didEarnMore = await talentLayerProvider.didUserMinimalEarnedOfToken(
+    userAddress,
+    minimalEarned,
+    tokenSymbol
+  );
+
+  return {
+    name: "talentlayer-earned-more-than",
+    timestamp: context.timestamp,
+    description:
+      "Find out if a user earned more than a certain amount of a token in total",
+    specs:
+      "Check to see if a user earned more than a certain amount of a token in total",
+    data: didEarnMore,
+    valueType: ValueType.Score,
+    tags: [Tags.User],
+  };
+};
+
 const generator: GroupGenerator = {
   generationFrequency: GenerationFrequency.Weekly,
 
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
     const contributorsGroup = await generateContributorsGroup(context);
     const usersGroup = await generateUsersGroup(context);
+    const didServiceGroup = await generateDidSellerServiceForBuyerGroup(
+      context,
+      "alice",
+      1
+    );
+    const didUserMinimalEarnedGroup =
+      await generateDidUserMinimalEarnedOfTokenGroup(
+        context,
+        "miguel",
+        0.001,
+        "MATIC"
+      );
+    const solidityGroup1 = await generateTopicGroup(context, "solidity", 1);
+    const ratingGroup5 = await generateRatingGroup(context, 5, 1);
 
-    return [contributorsGroup, usersGroup];
+    return [
+      contributorsGroup,
+      usersGroup,
+      didServiceGroup,
+      didUserMinimalEarnedGroup,
+      solidityGroup1,
+      ratingGroup5,
+    ];
   },
 };
 
--- group-generators/helpers/data-providers/talentlayer/types.ts
+++ group-generators/helpers/data-providers/talentlayer/types.ts
@@ -2,6 +2,39 @@ export type Users = {
   users: User[];
 };
 
+export type Services = {
+  services: Service[];
+};
+
+export type Reviews = {
+  reviews: Review[];
+};
+
+export type UserGains = {
+  userGains: UserGain[];
+};
+
 interface User {
   address: string;
+}
+
+interface Service {
+  id: string;
+  seller: User;
+}
+
+interface Review {
+  to: User;
+  service: Service;
+}
+
+export interface UserGain {
+  totalGain: string;
+  token: Token;
+  user: User;
+}
+
+interface Token {
+  symbol: string;
+  decimals: number;
 }
