61e4caf265bdfb025842177b4b9db2ce456a1ad7
Tomás Correia Marques <tomasmanuelcorreiamarques@gmail.com>
2023-03-17 22:50:31 +0000

    ETHPorto Hackathon: OnchainVerifier Data Provider (#1546)
    * feat: add onchain-verifier data provider
    
    * feat: update contract format
    
    * add example instance
    
    * fix: parse score as int
    
    * refactor to use mergeArguments
    
    * update instructions
    
    * Update types.ts
    
    * fix issue

---
+++ group-generators/helpers/data-providers/onchain-verifier/types.ts
@@ -0,0 +1,9 @@
+export type ContractConfig = {
+  // `network` is the network where the contract is deployed
+  // network: string;
+
+  // `url` is the address where the contract is deployed
+  // contractAddress: string;
+
+  mergedArguments: string;
+};
