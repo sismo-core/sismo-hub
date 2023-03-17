6d04139e38cd3336c2dd9661e4a37a56a9364f34
wslyvh <wslyvh@users.noreply.github.com>
2023-03-17 22:11:41 +0000

    update generator to throw

--- src/topics/group-generator/group-generator.ts
+++ src/topics/group-generator/group-generator.ts
@@ -151,10 +151,9 @@ export class GroupGeneratorService {
 
     const generator = this.groupGenerators[generatorName];
     if (!generator) {
-      this.logger.error(
+      throw new Error(
         `Group generator '${generatorName}' not found. Make sure the group generator exists.`
       );
-      return;
     }
     this.logger.info(
       `Generating group snapshots with generator (${generatorName})`
