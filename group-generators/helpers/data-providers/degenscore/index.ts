1177a98c51013f07189d91a17a5c5e512985cccb
mme <9083787+mme022@users.noreply.github.com>
2023-03-17 15:27:43 +0000

    added the first draft of functions

--- group-generators/helpers/data-providers/degenscore/index.ts
+++ group-generators/helpers/data-providers/degenscore/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 // eslint-disable-next-line no-restricted-imports
 import { TokenHolder } from "../transpose/types";
-import { UsersWithBadgesCountRequest, UsersCountRequest } from "./types";
-import { FetchedData } from "topics/group";
+//import { FetchedData } from "topics/group";
+import { UserBeaconData } from "./types";
 
 export class DegenScoreProvider {
   url: string;
@@ -13,125 +13,40 @@ export class DegenScoreProvider {
 
   public async getTokenHolders(): Promise<TokenHolder[]> {
     const { data: res } = await axios({
-      url: "https://api.etherscan.io/api?module=token&action=tokenholderlist&contractaddress=0x0521FA0bf785AE9759C7cB3CBE7512EbF20Fbdaa&page=1&offset=10&apikey=process.env.ETHERSCAN_API_KEY",
+      url:
+        "https://api.etherscan.io/api?module=token&action=tokenholderlist&contractaddress=0x0521FA0bf785AE9759C7cB3CBE7512EbF20Fbdaa&page=1&offset=10&apikey=" +
+        process.env.ETHERSCAN_API_KEY,
       method: "get",
     });
     return res.result;
   }
 
-  public async getDiscourse(endpoint: string): Promise<any> {
-    const { data: res } = await axios({
-      url: this.url + "/" + endpoint,
-      method: "get",
-      headers: this.headers,
+  public async getBeaconDataForHolders(): Promise<UserBeaconData[]> {
+    const tokenholders: TokenHolder[] = await this.getTokenHolders();
+
+    const userBeaconData: any[] = [];
+
+    tokenholders.forEach(async (holder) => {
+      const { data: res } = await axios({
+        url: "https://beacon.degenscore.com/v1/beacon/" + holder.owner_address,
+        method: "get",
+      });
+      userBeaconData.push({
+        owner_address: holder.owner_address,
+        quantity: holder.balance,
+        beaconData: res,
+      });
     });
-    return res;
+    return userBeaconData;
   }
 
-  public async getAllUsers(): Promise<any[]> {
-    try {
-      const res = await this.getDiscourse("admin/users/list/active.json");
-      const users: any[] = res.result.members;
-      return users;
-    } catch (error) {
-      throw new Error("Error fetching the users:" + error);
-    }
-  }
+  public async getBeaconHolders(score: string /*, trait: string*/) {
+    const userBeaconData: UserBeaconData[] =
+      await this.getBeaconDataForHolders();
 
-  public async getUsers(): Promise<FetchedData> {
-    const res: FetchedData = {};
-    try {
-      const users = await this.getAllUsers();
-
-      users.forEach((user) => {
-        res[user.id] = 1;
-      });
-
-      return res;
-    } catch (error) {
-      throw new Error("Error fetching the users: " + error);
-    }
-  }
-
-  public async getUsersCount(requestData: UsersCountRequest): Promise<number> {
-    this.url = requestData.discourseDomain;
-    this.headers = {
-      authorization: requestData.apiKey as string,
-      accept: "application/json",
-    };
-    const users = await this.getUsers();
-    return Object.keys(users).length;
-  }
-
-  public async getAllBadges(): Promise<any[]> {
-    try {
-      const res = await this.getDiscourse("admin/badges.json");
-      const badges: any[] = res.result.badges;
-      return badges;
-    } catch (error) {
-      throw new Error("Error fetching the badges: " + error);
-    }
-  }
-
-  public async getGroups(): Promise<any[]> {
-    try {
-      const res = await this.getDiscourse("admin/users/list/active.json");
-      const groups: any[] = res.result;
-      return groups;
-    } catch (error) {
-      throw new Error("Error fetching the groups: " + error);
-    }
-  }
-
-  public async getMembersGroup(id: number): Promise<any[]> {
-    try {
-      const res = await this.getDiscourse(`/groups/${id}/members.json`);
-      const members: any[] = res.result;
-      return members;
-    } catch (error) {
-      throw new Error("Error fetching the groups: " + error);
-    }
-  }
-
-  async getBadgesForUser(userName: string): Promise<any[]> {
-    try {
-      const res = await this.getDiscourse(`user-badges/${userName}.json`);
-      const badges: any[] = res.result.badges;
-      return badges;
-    } catch (error) {
-      throw new Error(
-        "Error fetching badges of users: " + userName + " with error " + error
-      );
-    }
-  }
-
-  public async getUsersWithBadges(
-    requestData: UsersWithBadgesCountRequest
-  ): Promise<FetchedData> {
-    const users: FetchedData = {};
-
-    const listOfUsers = await this.getAllUsers();
-
-    for (const user of listOfUsers) {
-      const badgesForUser = await this.getBadgesForUser(user.username);
-      badgesForUser.forEach((badges) => {
-        if (badges.id == requestData.badgeId) {
-          users[user.id] = 1;
-        }
-      });
-    }
-    return users;
-  }
-
-  public async getUsersWithBadgesCount(
-    requestData: UsersWithBadgesCountRequest
-  ): Promise<number> {
-    this.url = requestData.discourseDomain;
-    this.headers = {
-      authorization: requestData.apiKey as string,
-      accept: "application/json",
-    };
-    const users = await this.getUsersWithBadges(requestData);
-    return Object.keys(users).length;
+    const filter = userBeaconData.filter((elem: any) => {
+      elem["quantity"] >= score;
+    });
+    return filter;
   }
 }
--- group-generators/helpers/data-providers/degenscore/types.ts
+++ group-generators/helpers/data-providers/degenscore/types.ts
@@ -1,5 +1,3 @@
-// discourse api
-
 export type TokenHolder = {
   TokenHolderAddress: string;
   TokenHolderQuantity: string;
@@ -11,13 +9,41 @@ export type EtherscanResponse = {
   result: TokenHolder[];
 };
 
-export type UsersWithBadgesCountRequest = {
-  discourseDomain: string;
-  apiKey: string;
-  badgeId: number;
+export type DegenScoreTrait = {
+  name: string;
+  description: string;
+  image: string;
+  value: string | number;
+  valueType: string;
+  traitType: string;
+  rarity: string;
+  id: string;
+  tags: { id: string; value: string }[];
 };
 
-export type UsersCountRequest = {
-  discourseDomain: string;
-  apiKey: string;
+export type TraitInfoResponse = {
+  name: string;
+  description: string;
+  image: string;
+  properties: {
+    valueType: string;
+  };
+};
+
+export type BeaconInfoResponse = {
+  name: string;
+  description: string;
+  image: string;
+  properties: any;
+  updatedAt: string;
+  isConfirmed: boolean;
+  external_url: string;
+  animation_url: string;
+  traits: DegenScoreTrait;
+};
+
+export type UserBeaconData = {
+  owner_address: string;
+  balance: string;
+  beaconData: BeaconInfoResponse;
 };
