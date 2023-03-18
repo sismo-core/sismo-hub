fb3d2f54e266d3ea9c41235cb645d28785c34fa1
mme <9083787+mme022@users.noreply.github.com>
2023-03-18 09:55:19 +0000

    added group for degenscore

---
+++ group-generators/generators/degenscore-over-900/index.ts
@@ -0,0 +1,31 @@
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { GroupWithData, Tags, ValueType } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Weekly,
+
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    // Instantiate your snapshot provider
+    const degenscoreProvider = new dataProviders.DegenScoreProvider();
+    // Query all voters
+    const addresses = await degenscoreProvider.getBeaconOwnersWithScore(900);
+    return [
+      {
+        name: "degens-score-over-900",
+        description: "Get all degens with a score of minimum 900",
+        specs: "",
+        timestamp: context.timestamp,
+        data: addresses,
+        valueType: ValueType.Info,
+        tags: [Tags.User],
+      },
+    ];
+  },
+};
+
+export default generator;
