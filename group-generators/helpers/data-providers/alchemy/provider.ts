116552a604f3646a018ded9c072bd85c875e5c3a
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2023-03-18 01:08:52 +0000

    change token variable based on feedback

--- group-generators/helpers/data-providers/alchemy/provider.ts
+++ group-generators/helpers/data-providers/alchemy/provider.ts
@@ -5,7 +5,7 @@ import { FetchedData } from "topics/group";
 export class AlchemyProvider {
   url: string;
   private _alchemyAppToken: string | undefined;
-  constructor(alchemyAppToken = process.env.ALCHEMY_APP_TOKEN) {
+  constructor(alchemyAppToken = process.env.ALCHEMY_API_KEY) {
     this.url = "https://eth-mainnet.g.alchemy.com/nft/v2/";
     this._alchemyAppToken = alchemyAppToken;
   }
