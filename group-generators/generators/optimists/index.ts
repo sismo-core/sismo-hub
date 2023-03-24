1b79494c4bf8e952160bfabb92b54475a2a13fae
wslyvh <wslyvh@users.noreply.github.com>
2023-03-24 21:58:29 +0100

    allow for value scoring

--- group-generators/generators/optimists/index.ts
+++ group-generators/generators/optimists/index.ts
@@ -12,6 +12,7 @@ const generator: GroupGenerator = {
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
     const provider = new AttestationStationProvider();
     const receivers = await provider.getAttestations({
+      creator: "0xd870a73a32d0b8c34ccf1e6098e9a26977cb605b",
       key: "Flipside_user_scoring",
       value: "\u0005",
     });
@@ -24,7 +25,7 @@ const generator: GroupGenerator = {
           "Attest your Optimist score on-chain with a perfect score of 5.",
         specs: "Score a perfect 5 point score on the Flipside Optimist score",
         data: receivers,
-        valueType: ValueType.Info,
+        valueType: ValueType.Score,
         tags: [Tags.User, Tags.SybilResistance],
       },
     ];
