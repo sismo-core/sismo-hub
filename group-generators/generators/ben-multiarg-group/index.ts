923c1fa81ea05dc99f71521a5cf037816d72e023
Wesley <25974464+wslyvh@users.noreply.github.com>
2023-03-26 22:37:39 +0200

    Update OP ATST to allow for value scoring (#1606)
    * allow for value scoring
    
    * add creator to external atst group
    
    * update optimist group descriptions
    
    * update functions and add to atst provider
    
    * update optimists ValueType
    
    * correct interface args
    
    ---------
    
    Co-authored-by: wslyvh <wslyvh@users.noreply.github.com>

--- group-generators/generators/ben-multiarg-group/index.ts
+++ group-generators/generators/ben-multiarg-group/index.ts
@@ -1,4 +1,3 @@
-
 import { dataOperators } from "@group-generators/helpers/data-operators";
 import { dataProviders } from "@group-generators/helpers/data-providers";
 import { Tags, ValueType, GroupWithData } from "topics/group";
@@ -11,25 +10,26 @@ import {
 // Generated from factory.sismo.io
 
 const generator: GroupGenerator = {
-  
   generationFrequency: GenerationFrequency.Once,
-  
+
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
-  
-    const attestationStationProvider = new dataProviders.AttestationStationProvider();
-    
-    const attestationStationProviderData0 = await attestationStationProvider.getAttestations({
-      key: "numFollowersTwitter",
-      value: "1"
-    });
-    
+    const attestationStationProvider =
+      new dataProviders.AttestationStationProvider();
+
+    const attestationStationProviderData0 =
+      await attestationStationProvider.getAttestations({
+        creator: "0xd34a8775d06d41b36054d59ef2d09a79b7aa1fa2",
+        key: "numFollowersTwitter",
+        value: "1",
+      });
+
     const jsonListData1 = {
       "ben.eth": "1",
     };
-    
+
     const dataUnion = dataOperators.Union([
       attestationStationProviderData0,
-      jsonListData1 
+      jsonListData1,
     ]);
 
     return [
