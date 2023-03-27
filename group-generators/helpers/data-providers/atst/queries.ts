730f56718299ae32b47b6728012c1dabcc843674
wslyvh <wslyvh@users.noreply.github.com>
2023-03-27 10:45:09 +0200

    parse attestation values

--- group-generators/helpers/data-providers/atst/queries.ts
+++ group-generators/helpers/data-providers/atst/queries.ts
@@ -1,12 +1,16 @@
+import { BigNumber, utils } from "ethers";
 import { gql } from "graphql-request";
 import { GetAttestationDataType, QueryParams } from "./types";
 import { GraphQLProvider } from "@group-generators/helpers/data-providers/graphql";
+
+export type WagmiBytes = `0x${string}`;
 
 export const defaultLimit = 1000;
 export const getAttestationsQuery = async (
   graphqlProvider: GraphQLProvider,
   params: QueryParams
 ): Promise<GetAttestationDataType> => {
+  const value = params.value ? parseValue(params.value) : "";
   return graphqlProvider.query<GetAttestationDataType>(
     gql`
       {
@@ -15,10 +19,19 @@ export const getAttestationsQuery = async (
           orderBy: index
           orderDirection: asc
           where: { 
-            creator: "${params.creator}"
-            ${params.key ? `keyString: "${params.key}"` : ""}
-            ${params.value ? `valueString: "${params.value}"` : ""}
-            index_gte: ${params.index ?? 0}
+            and: [
+              { creator: "${params.creator}" }
+              { index_gte: ${params.index ?? 0} }
+              ${params.key ? `{ keyString: "${params.key}" }` : ""}
+              ${
+                params.value
+                  ? `{ or: [
+                  { val: "${value}" }
+                  { valueString: "${params.value}" }
+              ]}`
+                  : ""
+              }
+            ]
           }
         ) {
           id
@@ -33,3 +46,33 @@ export const getAttestationsQuery = async (
     `
   );
 };
+
+function parseValue(input: string): WagmiBytes {
+  input = input === "0x" ? "0x0" : input;
+  if (BigNumber.isBigNumber(input)) {
+    console.log("BigNumber");
+    return input.toHexString() as WagmiBytes;
+  }
+  if (typeof input === "number" || !isNaN(Number(input))) {
+    console.log("number");
+    return BigNumber.from(input).toHexString() as WagmiBytes;
+  }
+  if (typeof input === "boolean") {
+    console.log("boolean");
+    return input ? "0x1" : "0x0";
+  }
+  if (utils.isAddress(input)) {
+    console.log("address");
+    return input as WagmiBytes;
+  }
+  if (utils.isHexString(input)) {
+    console.log("hexString");
+    return input as WagmiBytes;
+  }
+  if (typeof input === "string") {
+    console.log("string");
+    return utils.hexlify(utils.toUtf8Bytes(input)) as WagmiBytes;
+  }
+
+  throw new Error(`unrecognized value type '${input}'`);
+}
