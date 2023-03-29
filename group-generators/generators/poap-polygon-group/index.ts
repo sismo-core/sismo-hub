47fafd703052e8e11349d29711a2d3a5120908d0
vera <119699508+aloeveruschka@users.noreply.github.com>
2023-03-29 16:41:00 -0700

    participant update 27/3/23 (#1621)
    added participants #27 to 32

--- group-generators/generators/poap-polygon-group/index.ts
+++ group-generators/generators/poap-polygon-group/index.ts
@@ -23,7 +23,13 @@ const generator: GroupGenerator = {
             "0xd0707F17a40Cb505bA3B85146990738424aac7FA": "1",
             "0x7c16eCE9f1a4De5a3AFa455492e3AFd9c98F36bE": "1",
             "darrylyeo.eth": "1",
-            "0xe7910F0b83ad155737043c771E2594f74B0BB739": "1"
+            "0xe7910F0b83ad155737043c771E2594f74B0BB739": "1",
+            "0x44B8c9B3a9eBBf768DDa0534635C4C1379F23FAa": "1",
+            "0xceCD7f69dB87E15264c82c253B648E624dD2aB93": "1",
+            "0x840D788ac6fFcc80CbEaf3BA8Fe9bB7288Fcee5A": "1",
+            "0xcF8e7573894CB97D2589EFdaF04cf65b8ad94cD9": "1",
+            "0x295f41411Be5087E7bF2f8b6e5f8d744FA068Cb5": "1",
+            "0x87C870fAc12951196188149192a8310013989E3d": "1",
         };
 
         return [
