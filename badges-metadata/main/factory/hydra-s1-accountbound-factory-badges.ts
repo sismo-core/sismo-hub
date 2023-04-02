166cb48258a680c219b6ecda1875cdc9e0a47392
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-04-02 20:05:06 +0200

    [factory]: Adding new af group and its ZK Badge (#1640)

--- badges-metadata/main/factory/hydra-s1-accountbound-factory-badges.ts
+++ badges-metadata/main/factory/hydra-s1-accountbound-factory-badges.ts
@@ -9468,6 +9468,32 @@ export const factoryBadges: BadgeMetadata[] = [
     links: []
   },
   {
+    internalCollectionId: 2979278,
+    networks: [
+      Network.Goerli,
+      Network.Mumbai
+    ],
+    name: "AF",
+    description: "Goverance of Boringness",
+    image: "af.svg",
+    groupSnapshot: {
+      groupName: "af"
+    },
+    publicContacts: [
+      {
+        type: "twitter",
+        contact: "@bxldao"
+      }
+    ],
+    links: [
+      {
+        logoUrl: "",
+        url: "https://rumi.zkft.be",
+        label: "website"
+      }
+    ]
+  },
+  {
     internalCollectionId: 2982194,
     networks: [
       Network.Gnosis
