298e298a6ca5145daf53317c2c2c4f0c6a43b312
Sam Polgar <sampolgar@gmail.com>
2023-04-08 11:22:04 +1000

    Revert "group generator"
    This reverts commit 9777774ac8dac4277a08664a5bdc58df341f6c54.

--- src/api/api.ts
+++ src/api/api.ts
@@ -16,7 +16,6 @@ import dataProviderInterfacesRoutes from "topics/data-provider/data-provider.api
 import { FlowService } from "topics/flow";
 import flowsRoutes from "topics/flow/flow.api";
 import { GroupStore } from "topics/group";
-import groupsRoutes from "topics/group/group.api";
 import {
   GroupGeneratorService,
   GroupGeneratorStore,
@@ -24,6 +23,7 @@ import {
 import groupGeneratorsRoutes from "topics/group-generator/group-generator.api";
 import { GroupSnapshotStore } from "topics/group-snapshot";
 import groupSnapshotsRoutes from "topics/group-snapshot/group-snapshot.api";
+import groupsRoutes from "topics/group/group.api";
 import { RegistryTreeService } from "topics/registry-tree";
 
 const removeTrailingSlash = (s: string) => s.replace(/\/+$/, "");
