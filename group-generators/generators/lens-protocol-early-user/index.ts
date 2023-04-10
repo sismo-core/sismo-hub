12f8f597c201d8b1079191d101fbcc1dd720abca
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-04-10 07:51:46 +0200

    [factory]: Adding new lens-protocol-early-user group and its ZK Badge

---
+++ group-generators/generators/lens-protocol-early-user/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Daily,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const lensProvider = new dataProviders.LensProvider();
+    
+    const lensProviderData0 = await lensProvider.getWhoCollectedPublication({
+      publicationId: "0x01-0x01"
+    });
+
+    return [
+      {
+        name: "lens-protocol-early-user",
+        timestamp: context.timestamp,
+        description: "Collected first post on the Lens Protocol",
+        specs: "",
+        data: lensProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
