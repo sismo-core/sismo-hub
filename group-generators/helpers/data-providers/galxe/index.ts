099d2d001066015c69bc014c72295b34b45d681d
Sam Polgar <sampolgar@gmail.com>
2023-04-10 21:21:03 +1000

    galxe data provider

---
+++ group-generators/helpers/data-providers/galxe/index.ts
@@ -0,0 +1,49 @@
+import { gql } from "graphql-request";
+import {
+  QueryCampaignOutput,
+  QueryCampaignInput,
+  IGalxeProvider,
+} from "./types";
+
+import { GraphQLProvider } from "@group-generators/helpers/data-providers/graphql";
+import { FetchedData } from "topics/group";
+
+export class GalxeProvider extends GraphQLProvider implements IGalxeProvider {
+  constructor() {
+    super({ url: "https://graphigo.prd.galaxy.eco/query" });
+  }
+
+  public async getCampaignHolders(
+    input: QueryCampaignInput
+  ): Promise<FetchedData> {
+    const holders: FetchedData = {};
+    let after = "0";
+    let campaign: QueryCampaignOutput["campaign"];
+
+    do {
+      const query = gql`
+        query Campaign {
+        campaign(id: "${input.id}") {
+          id
+          name
+          status
+          numNFTMinted
+          holdersList(first: 1000, after: "${after}")
+        }
+      }
+    `;
+
+      const response: QueryCampaignOutput =
+        await this.query<QueryCampaignOutput>(query);
+
+      campaign = response.campaign;
+      for (const holder of campaign.holdersList) {
+        holders[holder] = 1;
+      }
+
+      after = campaign.holdersList.slice(-1)[0];
+    } while (Object.keys(holders).length < campaign.numNFTMinted);
+
+    return holders;
+  }
+}
---
+++ group-generators/helpers/data-providers/galxe/types.ts
@@ -0,0 +1,20 @@
+import { IGraphQLProvider } from "@group-generators/helpers/data-providers/graphql/types";
+import { FetchedData } from "topics/group";
+
+export type QueryCampaignOutput = {
+  campaign: {
+    id: string;
+    name: string;
+    status: string;
+    numNFTMinted: number;
+    holdersList: string[];
+  };
+};
+
+export type QueryCampaignInput = {
+  id: string;
+};
+
+export interface IGalxeProvider extends IGraphQLProvider {
+  getCampaignHolders(input: QueryCampaignInput): Promise<FetchedData>;
+}
