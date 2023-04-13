652cbb8e5082db5390ef906f1a87c9e3f5bf4d9c
Sam Polgar <sampolgar@gmail.com>
2023-04-13 23:08:00 +1000

    cyberconnect holders group

---
+++ group-generators/generators/example-cyberconnect/index.ts
@@ -0,0 +1,33 @@
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { GroupWithData, Tags, ValueType } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Weekly,
+
+  //CyberConnect holders are on BSC contract: https://bscscan.com/token/0x2723522702093601e6360cae665518c4f63e9da6
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    const ankrProvider = new dataProviders.AnkrProvider();
+    const addresses = await ankrProvider.getNftHolders({
+      network: "bsc",
+      address: "0x2723522702093601e6360CAe665518C4f63e9dA6",
+    });
+    return [
+      {
+        name: "example-cyberconnect",
+        description: "Get all CyberConnect holders",
+        specs: "",
+        timestamp: context.timestamp,
+        data: addresses,
+        valueType: ValueType.Info,
+        tags: [Tags.User],
+      },
+    ];
+  },
+};
+
+export default generator;
--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -145,6 +145,7 @@ import etheriumEthBit5890 from "./etherium-eth-bit-5890";
 import ethermail from "./ethermail";
 import ethporto from "./ethporto";
 import everwaveFounder from "./everwave-founder";
+import exampleCyberConnect from "./example-cyberconnect";
 import exampleGalxePassport from "./example-galxepassport"
 import exampleUnlock from "./example-unlock";
 import fantomAlphaRoundDonaters from "./fantom-alpha-round-donaters";
@@ -710,6 +711,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "ethporto": ethporto,
   "everwave-founder": everwaveFounder,
   "example-galxe-passport": exampleGalxePassport,
+  "example-cyberconnect": exampleCyberConnect,
   "example-unlock": exampleUnlock,
   "fantom-alpha-round-donaters": fantomAlphaRoundDonaters,
   "farmer": farmer,
