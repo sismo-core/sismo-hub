d939954baa06f3a7003d0ca396991b5acf4702ee
MartinGbz <martin.grabarz@outlook.fr>
2023-04-16 14:20:42 +0200

    fix: space group index

--- group-generators/generators/free-range-validators/index.ts
+++ group-generators/generators/free-range-validators/index.ts
@@ -1,33 +1,33 @@
 import { dataProviders } from "@group-generators/helpers/data-providers";
 import { GroupWithData, Tags, ValueType } from "topics/group";
 import {
-    GenerationContext,
-    GenerationFrequency,
-    GroupGenerator,
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
 } from "topics/group-generator";
 
 const generator: GroupGenerator = {
-    generationFrequency: GenerationFrequency.Once,
-    generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
-        const restProvider = new dataProviders.RestProvider();
-        const validators = await restProvider.getAccountsFromAPI({
-            url: "http://freerangedao.xyz/api/allowlist",
-            method: "GET",
-        });
+  generationFrequency: GenerationFrequency.Once,
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    const restProvider = new dataProviders.RestProvider();
+    const validators = await restProvider.getAccountsFromAPI({
+      url: "http://freerangedao.xyz/api/allowlist",
+      method: "GET",
+    });
 
-        return [
-            {
-                name: "free-range-validators",
-                specs: "Validators on our allowlist",
-                description:
-                    "Validators eligible to take the Free Range Survey",
-                timestamp: context.timestamp,
-                data: validators,
-                valueType: ValueType.Score,
-                tags: [Tags.User],
-            },
-        ];
-    },
+    return [
+      {
+        name: "free-range-validators",
+        specs: "Validators on our allowlist",
+        description:
+          "Validators eligible to take the Free Range Survey",
+        timestamp: context.timestamp,
+        data: validators,
+        valueType: ValueType.Score,
+        tags: [Tags.User],
+      },
+    ];
+  },
 };
 
 export default generator;
