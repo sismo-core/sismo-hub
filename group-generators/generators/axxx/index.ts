476fc5368185b1cfa071faf8f2dcdc198a62d9e1
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-04-23 12:41:39 +0900

    [factory]: Adding new axxx group

---
+++ group-generators/generators/axxx/index.ts
@@ -0,0 +1,43 @@
+
+import { dataOperators } from "@group-generators/helpers/data-operators";
+import { Tags, ValueType, GroupWithData, GroupStore } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  dependsOn: ["b1y3-og-voters"],
+  
+  generate: async (context: GenerationContext, groupStore: GroupStore): Promise<GroupWithData[]> => {
+  
+    
+    const b1y3OgVotersGroupLatest = await groupStore.latest(
+      "b1y3-og-voters"
+    );
+    
+    const b1y3OgVotersData0 = dataOperators.Map(
+      await b1y3OgVotersGroupLatest.data(),
+      1
+    );
+
+    return [
+      {
+        name: "axxx",
+        timestamp: context.timestamp,
+        description: "1",
+        specs: "1",
+        data: b1y3OgVotersData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
