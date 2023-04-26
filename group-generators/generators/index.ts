cfc1592cac03ee14e3c6344c536d041d23ece88e
Sam Polgar <sampolgar@gmail.com>
2023-04-27 02:03:27 +1000

    groupgen: aavechan delegators (#1727)

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -3,6 +3,7 @@ import { GroupGeneratorsLibrary } from "topics/group-generator";
 import OxlegionLensFollower from "./0xlegion-lens-follower";
 import OxlinkLensFollowers from "./0xlink-lens-followers";
 import a01kAlpha from "./a01k-alpha";
+import aavechanAbove10 from "./aavechan-above10"
 import abuseGuardian from "./abuse-guardian";
 import aeraByOnefootballOgs from "./aera-by-onefootball-ogs";
 import af from "./af";
@@ -585,6 +586,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "0xlink-lens-followers": OxlinkLensFollowers,
   "996-icu": icu996,
   "a01k-alpha": a01kAlpha,
+  "aavechan-above10": aavechanAbove10,
   "abuse-guardian": abuseGuardian,
   "aera-by-onefootball-ogs": aeraByOnefootballOgs,
   "af": af,
