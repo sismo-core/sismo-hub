862d9172b18e9fe77cfe55bc51fe4b8452718117
MartinGbz <martin.grabarz@outlook.fr>
2023-04-27 17:22:56 +0200

    switch daily to once static groups + switch daily to weekly zk-developer depending groups (#1733)

--- group-generators/generators/justsometest/index.ts
+++ group-generators/generators/justsometest/index.ts
@@ -10,7 +10,7 @@ import {
 
 const generator: GroupGenerator = {
   
-  generationFrequency: GenerationFrequency.Daily,
+  generationFrequency: GenerationFrequency.Once,
   
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
   
