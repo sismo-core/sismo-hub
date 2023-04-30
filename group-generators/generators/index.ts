3350a395b5a82fae4b49105ea90526b6b1b45539
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-05-01 06:12:59 +0900

    [factory]: Adding new raverz group and its ZK Badge

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -389,6 +389,7 @@ import ra from "./ra";
 import rahulkrLensFollowers from "./rahulkr-lens-followers";
 import ratra from "./ratra";
 import raul from "./raul";
+import raverz from "./raverz";
 import raviZkBadgeOnSismo from "./ravi-zk-badge-on-sismo";
 import rayS from "./ray-s";
 import regenesisNftHolders from "./regenesis-nft-holders";
@@ -976,6 +977,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "rahulkr-lens-followers": rahulkrLensFollowers,
   "ratra": ratra,
   "raul": raul,
+  "raverz": raverz,
   "ravi-zk-badge-on-sismo": raviZkBadgeOnSismo,
   "ray-s": rayS,
   "regenesis-nft-holders": regenesisNftHolders,
---
+++ group-generators/generators/raverz/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Weekly,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const tokenProvider = new dataProviders.TokenProvider();
+    
+    const tokenProviderData0 = await tokenProvider.getNftHolders({
+      contractAddress: "0xE03d5E1d942A47f222A227bff4AD933B1c43a675"
+    });
+
+    return [
+      {
+        name: "raverz",
+        timestamp: context.timestamp,
+        description: "hold a RAVERZ.XYZ raver",
+        specs: "This badge is for holders of RAVERZ.XYZ raverz.",
+        data: tokenProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
