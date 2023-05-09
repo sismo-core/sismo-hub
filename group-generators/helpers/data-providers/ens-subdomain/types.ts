ffe953a285d2f356a3b44ee3c38f740eab8052b9
Sam Polgar <sampolgar@gmail.com>
2023-05-10 07:02:50 +1000

    adding thegraph ens subdomain

---
+++ group-generators/helpers/data-providers/ens-subdomain/types.ts
@@ -0,0 +1,56 @@
+import { ISubgraphProvider } from "@group-generators/helpers/data-providers/subgraph";
+import { FetchedData } from "topics/group";
+
+export type EnsSubdomainsResponse = {
+  domains: [
+    {
+      name: string;
+      subdomains: [
+        {
+          owner: {
+            id: string;
+          };
+        }
+      ];
+    }
+  ];
+};
+
+export type EnsDomainParams = { subdomain: string };
+
+export interface IEnsSubdomainsSubgraphProvider extends ISubgraphProvider {
+  getEnsHolders(params: EnsDomainParams): Promise<FetchedData>;
+}
+
+// query Domains {
+//     domains(where: {labelName: "sismo"}) {
+//         name
+//         subdomains(first: 1000) {
+//             owner {
+//                 id
+//             }
+//         }
+//     }
+// }
+
+// {
+//     "data": {
+//         "domains": [
+//             {
+//                 "name": "sismo.eth",
+//                 "subdomains": [
+//                     {
+//                         "owner": {
+//                             "id": "0xef218cb373743770f7096baad714b6a979bee2ed"
+//                         }
+//                     },
+//                     {
+//                         "owner": {
+//                             "id": "0xc4798b79d22630cee83b4ecb0fd98cd5ff0fbb62"
+//                         }
+//                     },
+//                     {
+//                         "owner": {
+//                             "id": "0xf4b0400eb76fb71e111322360cea2efdc6be0bfb"
+//                         }
+//                     },
