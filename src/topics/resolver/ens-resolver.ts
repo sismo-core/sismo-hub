f874ab876a3529d5bfe7346da5092e204be3af7c
MartinGbz <martin.grabarz@outlook.fr>
2023-05-09 16:33:29 +0200

    feat: fix & improve ENS Resolver

--- src/topics/resolver/ens-resolver.ts
+++ src/topics/resolver/ens-resolver.ts
@@ -2,6 +2,7 @@
 import { ethers } from "ethers";
 import { gql } from "graphql-request";
 import { IResolver } from "./resolver";
+import { withConcurrency } from "./utils";
 import { domain } from "@group-generators/helpers/data-providers/ens/types";
 import { GraphQLProvider } from "@group-generators/helpers/data-providers/graphql";
 import { JsonRpcProvider } from "@group-generators/helpers/data-providers/json-rpc";
@@ -21,14 +22,45 @@ export class EnsResolver extends GraphQLProvider implements IResolver {
       : (this.provider = ethers.getDefaultProvider());
   }
 
+  resolvedAccounts: FetchedData = {};
+
   public async resolve(ensDataArray: FetchedData): Promise<FetchedData> {
-    const ensData = Object.keys(ensDataArray)[0];
+    const ensData = Object.keys(ensDataArray);
+
+    const resolveENSData = async (ensData: string[]): Promise<void> => {
+      const userData = await this.resolveLensHandlesQuery(ensData);
+
+      const resolvedAccounts = {} as FetchedData;
+
+      for (const user of userData) {
+        if (user.resolvedAddress !== null) {
+          resolvedAccounts[user.resolvedAddress.id] = ensDataArray[user.name];
+        } else {
+          const retryResolved = await this.resolveEnsFromJsonRpc(user.name);
+          if (retryResolved) {
+            resolvedAccounts[retryResolved] = ensDataArray[user.name];
+          }
+        }
+      }
+
+      Object.assign(this.resolvedAccounts, resolvedAccounts);
+    };
+
+    await withConcurrency(ensData, resolveENSData, {
+      batchSize: 5,
+      concurrency: 10,
+    });
+
+    return this.resolvedAccounts;
+  }
+
+  public async resolveLensHandlesQuery(ensData: string[]): Promise<domain[]> {
     const domains = await this.query<{
       domains: domain[];
     }>(
       gql`
-        query getDomain($ensName: String) {
-          domains(where: { name: $ensName }) {
+        query getDomain($ensNames: [String!]) {
+          domains(where: { name_in: $ensNames }) {
             name
             resolvedAddress {
               id
@@ -36,36 +68,25 @@ export class EnsResolver extends GraphQLProvider implements IResolver {
           }
         }
       `,
-      { ensName: ensData }
+      { ensNames: ensData }
     );
-    const userData = domains.domains[0];
-
-    try {
-      return { [userData.resolvedAddress.id]: Object.values(ensDataArray)[0] };
-    } catch (error) {
-      // ens user address is not in the subgraph, calling ENS Registry with ethers
-      return this.resolveEnsFromJsonRpc(ensDataArray);
-    }
+    return domains.domains;
   }
 
-  public async resolveEnsFromJsonRpc(ens: FetchedData): Promise<FetchedData> {
+  public async resolveEnsFromJsonRpc(ens: string): Promise<string> {
     // another try to prevent this type of invalid address https://etherscan.io/enslookup-search?search=karl.floersch.eth
     try {
       const resolvedAddress: string | null = await this.provider.resolveName(
-        Object.keys(ens)[0]
+        ens
       );
       if (resolvedAddress === null) {
-        return {
-          "0x0000000000000000000000000000000000000000": Object.values(ens)[0],
-        };
+        return "";
       }
-      return { [resolvedAddress]: Object.values(ens)[0] };
+      return resolvedAddress;
     } catch (error) {
       console.log(`invalid address for ${ens}`);
       // invalid address for ensUser.name
-      return {
-        "0x0000000000000000000000000000000000000000": Object.values(ens)[0],
-      };
+      return "";
     }
   }
 }
