7a85a06067cbc5cabbb9e8b10203f3bae723481c
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-05-14 06:10:38 +0900

    [factory]: Adding new hold-ape group (#1800)

---
+++ group-generators/generators/hold-ape/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const tokenProvider = new dataProviders.TokenProvider();
+    
+    const tokenProviderData0 = await tokenProvider.getERC20Holders({
+      contractAddress: "0x4d224452801ACEd8B2F0aebE155379bb5D594381"
+    });
+
+    return [
+      {
+        name: "hold-ape",
+        timestamp: context.timestamp,
+        description: "All the holders of the APE token as of 13/5/2022",
+        specs: "Hold a APE Coin and listen to one of the best songs in Town in QSound",
+        data: tokenProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
