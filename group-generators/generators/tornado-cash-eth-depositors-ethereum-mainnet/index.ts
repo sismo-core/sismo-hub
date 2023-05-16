c85e1df1c352ab07f0cf7fc1016e208baf0ce38a
MartinGbz <martin.grabarz@outlook.fr>
2023-05-16 19:55:26 +0200

    Add tornado cash depositor group (#1812)

---
+++ group-generators/generators/tornado-cash-eth-depositors-ethereum-mainnet/index.ts
@@ -0,0 +1,121 @@
+import { BigNumber } from "ethers";
+import { dataOperators } from "@group-generators/helpers/data-operators";
+import {BigQueryProvider} from "@group-generators/helpers/data-providers/big-query/big-query";
+
+import {
+  ValueType,
+  Tags,
+  FetchedData,
+  GroupWithData,
+  AccountSource,
+} from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Daily,
+
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+    const bigQueryProvider = new BigQueryProvider();
+
+    // Mainnet Tornado Cash contract address: 0.1ETH
+    const tornadoCashDepositors01ETH = "0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc";
+    // Mainnet Tornado Cash contract address: 1ETH
+    const tornadoCashDepositors1ETH = "0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936";
+    // Mainnet Tornado Cash contract address: 10ETH
+    const tornadoCashDepositors10ETH = "0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF";
+    // Mainnet Tornado Cash contract address: 100ETH
+    const tornadoCashDepositors100ETH = "0xA160cdAB225685dA1d56aa342Ad8841c3b53f291";
+
+    const depositFunctionEthABI =
+    "function deposit(bytes32 _commitment) external payable nonReentrant";
+    type DepositFunction = {
+    _commitment: string;
+    };
+
+    const multiplier = BigNumber.from(10).pow(18);
+
+    // Get all 0.1ETH Deposits on Tornado Cash
+    const getTornadoCash01DepositTransactions =
+    await bigQueryProvider.getAllTransactionsForSpecificMethod<DepositFunction>(
+      {
+        functionABI: depositFunctionEthABI,
+        contractAddress: tornadoCashDepositors01ETH,
+      }
+    );
+    const ethereum01Depositors: FetchedData = {};
+    getTornadoCash01DepositTransactions.forEach((transaction) => {
+      // 0.1 ETH denominated in wei
+      // 0.1 ETH is 10^17 wei
+      ethereum01Depositors[transaction.from] = BigNumber.from(10).pow(17).toString();
+    });
+
+    // Get all 1ETH Deposits on Tornado Cash
+    const getTornadoCash1DepositTransactions =
+    await bigQueryProvider.getAllTransactionsForSpecificMethod<DepositFunction>(
+      {
+        functionABI: depositFunctionEthABI,
+        contractAddress: tornadoCashDepositors1ETH,
+      }
+    );
+    const ethereum1Depositors: FetchedData = {};
+    getTornadoCash1DepositTransactions.forEach((transaction) => {
+      // 1 ETH denominated in wei
+      ethereum1Depositors[transaction.from] = BigNumber.from(1).mul(multiplier).toString();
+    });
+
+    // Get all 10ETH Deposits on Tornado Cash
+    const getTornadoCash10DepositTransactions =
+    await bigQueryProvider.getAllTransactionsForSpecificMethod<DepositFunction>(
+      {
+        functionABI: depositFunctionEthABI,
+        contractAddress: tornadoCashDepositors10ETH,
+      }
+    );
+    const ethereum10Depositors: FetchedData = {};
+    getTornadoCash10DepositTransactions.forEach((transaction) => {
+      // 10 ETH denominated in wei
+      ethereum10Depositors[transaction.from] = BigNumber.from(10).mul(multiplier).toString();
+    });
+
+    // Get all 100ETH Deposits on Tornado Cash
+    const getTornadoCash100DepositTransactions =
+    await bigQueryProvider.getAllTransactionsForSpecificMethod<DepositFunction>(
+      {
+        functionABI: depositFunctionEthABI,
+        contractAddress: tornadoCashDepositors100ETH,
+      }
+    );
+    const ethereum100Depositors: FetchedData = {};
+    getTornadoCash100DepositTransactions.forEach((transaction) => {
+      // 100 ETH denominated in wei
+      ethereum100Depositors[transaction.from] = BigNumber.from(100).mul(multiplier).toString();
+    });
+    
+
+    const tornadoCashDepositors = dataOperators.Union([
+      ethereum01Depositors,
+      ethereum1Depositors,
+      ethereum10Depositors,
+      ethereum100Depositors
+    ]);
+
+    return [
+      {
+        name: "tornado-cash-eth-depositors-ethereum-mainnet",
+        timestamp: context.timestamp,
+        description: "All ETH Tornado Cash depositors on Ethereum mainnet",
+        specs: "Deposit 0.1 or 1 or 10 or 100 ETH on Tornado Cash on Ethereum mainnet",
+        data: tornadoCashDepositors,
+        accountSources: [AccountSource.ETHEREUM],
+        valueType: ValueType.Score,
+        tags: [Tags.Privacy, Tags.User],
+      },
+    ];
+  },
+};
+
+export default generator;
