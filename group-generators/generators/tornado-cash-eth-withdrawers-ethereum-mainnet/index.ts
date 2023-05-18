2614e3d59a2695c821613a59928f577811c2c3c0
MartinGbz <martin.grabarz@outlook.fr>
2023-05-18 19:47:14 +0200

    fix: router withdrawers

--- group-generators/generators/tornado-cash-eth-withdrawers-ethereum-mainnet/index.ts
+++ group-generators/generators/tornado-cash-eth-withdrawers-ethereum-mainnet/index.ts
@@ -22,6 +22,43 @@ const generator: GroupGenerator = {
 
     const ethWithdrawers: FetchedData = {};
 
+    // Mainnet Tornado Cash contract address: 0.1ETH
+    const tornadoCashPool01ETH = "0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc";
+    // Mainnet Tornado Cash contract address: 1ETH
+    const tornadoCashPool1ETH = "0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936";
+    // Mainnet Tornado Cash contract address: 10ETH
+    const tornadoCashPool10ETH = "0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF";
+    // Mainnet Tornado Cash contract address: 100ETH
+    const tornadoCashPool100ETH = "0xA160cdAB225685dA1d56aa342Ad8841c3b53f291";
+
+    const multiplier = BigNumber.from(10).pow(18);
+
+    const getNewBalance = (address: string, newWithdraw: number) => {
+      // Get the current balance of the address in wei
+      const curBalance = ethWithdrawers[address] ? BigNumber.from(ethWithdrawers[address]) : BigNumber.from(0);
+      // Convert the new deposit to wei
+      const newWithdrawWei = (newWithdraw === 0.1) ? BigNumber.from(10).pow(17) : BigNumber.from(newWithdraw).mul(multiplier);
+      // Return the new balance as a string in wei
+      return curBalance.add(newWithdrawWei).toString();
+    };
+
+    const setCorrespondingBalance = (pool: string, address: string): void => {
+      switch(pool) {
+        case tornadoCashPool01ETH:
+          ethWithdrawers[address] = getNewBalance(address, 0.1);
+          break;
+        case tornadoCashPool1ETH:
+          ethWithdrawers[address] = getNewBalance(address, 1);
+          break;
+        case tornadoCashPool10ETH:
+          ethWithdrawers[address] = getNewBalance(address, 10);
+          break;
+        case tornadoCashPool100ETH:
+          ethWithdrawers[address] = getNewBalance(address, 100);
+          break;
+      }
+    }
+
     // #########################################################
     // # GET TORNADO CASH WITHDRAWERS FROM OLD ROUTER CONTRACT #
     // #########################################################
@@ -45,12 +82,15 @@ const generator: GroupGenerator = {
       {
         functionABI: oldRouterWithdrawFunctionABI,
         contractAddress: tornadoCashOldRouter,
+        options: {
+          functionArgs: true
+        },
       }
     );
     
     for (const transaction of tornadoCashOldRouterWithdrawTransactions) {
-      if(transaction.value._hex !== "0x00") {
-        ethWithdrawers[transaction.from] = transaction.value.toString();
+      if(transaction?.args?._tornado) {
+        setCorrespondingBalance(transaction.args._tornado, transaction.from);
       }
     }
 
@@ -78,12 +118,15 @@ const generator: GroupGenerator = {
       {
         functionABI: routerWithdrawFunctionABI,
         contractAddress: tornadoCashRouter,
+        options: {
+          functionArgs: true
+        },
       }
     );
 
     for (const transaction of tornadoCashRouterWithdrawTransactions) {
-      if(transaction.value._hex !== "0x00") {
-        ethWithdrawers[transaction.from] = transaction.value.toString();
+      if(transaction?.args?._tornado) {
+        setCorrespondingBalance(transaction.args._tornado, transaction.from);
       }
     }
 
@@ -91,15 +134,6 @@ const generator: GroupGenerator = {
     // ###############################################
     // # GET TORNADO CASH WITHDRAWERS FROM ETH POOLS #
     // ###############################################
-
-    // Mainnet Tornado Cash contract address: 0.1ETH
-    const tornadoCashPool01ETH = "0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc";
-    // Mainnet Tornado Cash contract address: 1ETH
-    const tornadoCashPool1ETH = "0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936";
-    // Mainnet Tornado Cash contract address: 10ETH
-    const tornadoCashPool10ETH = "0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF";
-    // Mainnet Tornado Cash contract address: 100ETH
-    const tornadoCashPool100ETH = "0xA160cdAB225685dA1d56aa342Ad8841c3b53f291";
 
     const poolsWithdrawFunctionEthABI =
     "function withdraw(bytes calldata _proof, bytes32 _root, bytes32 _nullifierHash, address payable _recipient, address payable _relayer, uint256 _fee, uint256 _refund) external payable nonReentrant";
@@ -111,17 +145,6 @@ const generator: GroupGenerator = {
       _relayer: string;
       _fee: BigNumber;
       _refund: BigNumber;
-    };
-
-    const multiplier = BigNumber.from(10).pow(18);
-
-    const getNewBalance = (address: string, newWithdraw: number) => {
-      // Get the current balance of the address in wei
-      const curBalance = ethWithdrawers[address] ? BigNumber.from(ethWithdrawers[address]) : BigNumber.from(0);
-      // Convert the new deposit to wei
-      const newWithdrawWei = (newWithdraw === 0.1) ? BigNumber.from(10).pow(17) : BigNumber.from(newWithdraw).mul(multiplier);
-      // Return the new balance as a string in wei
-      return curBalance.add(newWithdrawWei).toString();
     };
 
     // Get all 0.1ETH Withdraws on Tornado Cash
