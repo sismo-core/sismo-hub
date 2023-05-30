83c606a67e561b3816a75d4742200e1e5c97b86b
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-05-30 14:53:04 +0200

    [factory]: Adding new dudulab group

---
+++ group-generators/generators/dudulab/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const tokenProvider = new dataProviders.TokenProvider();
+    
+    const tokenProviderData0 = await tokenProvider.getNftHolders({
+      contractAddress: "0x365D87a8d31c656ED1479C0F54F19E3BE9F19537"
+    });
+
+    return [
+      {
+        name: "dudulab",
+        timestamp: context.timestamp,
+        description: "DuDuHolders",
+        specs: "Hold DuDuLab NFT",
+        data: tokenProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
