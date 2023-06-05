49271cb0f0fac612c3cd63268e5cb415b269532c
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-06-05 15:48:18 +0200

    [factory]: Adding new macrodao-fellows group (#1869)

---
+++ group-generators/generators/macrodao-fellows/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const tokenProvider = new dataProviders.TokenProvider();
+    
+    const tokenProviderData0 = await tokenProvider.getNftHolders({
+      contractAddress: "0xcc84caaa141039c7fe4a6e259bb7c5dc1b6204f7"
+    });
+
+    return [
+      {
+        name: "macrodao-fellows",
+        timestamp: context.timestamp,
+        description: "Data Group of Macro Grads",
+        specs: "Hold a Macro Fellowship NFT",
+        data: tokenProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
