6a69bd74ffb3f1bce5643cb88b751b3a34b1555b
MartinGbz <martin.grabarz@outlook.fr>
2023-06-07 16:01:55 +0200

    feat: sismo-core-team + sismo-genesis-team gen freq to once

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -496,6 +496,7 @@ import sismoContributors from "./sismo-contributors"
 import sismoContributorsTier1Users from "./sismo-contributors-tier1-users"
 import sismoContributorsTier2ImpactfulContributors from "./sismo-contributors-tier2-impactful-contributors"
 import sismoContributorsTier3Builders from "./sismo-contributors-tier3-builders"
+import sismoCoreTeam from "./sismo-core-team";
 import sismoDiggers from "./sismo-diggers";
 import sismoDomain from "./sismo-domains";
 import sismoEarlyCommunityMembers from "./sismo-early-community-members"
@@ -1162,6 +1163,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "sismo-contributors-tier1-users": sismoContributorsTier1Users,
   "sismo-contributors-tier2-impactful-contributors": sismoContributorsTier2ImpactfulContributors,
   "sismo-contributors-tier3-builders": sismoContributorsTier3Builders,
+  "sismo-core-team": sismoCoreTeam, 
   "sismo-diggers": sismoDiggers,
   "sismo-domains": sismoDomain,
   "sismo-early-community-members": sismoEarlyCommunityMembers,
