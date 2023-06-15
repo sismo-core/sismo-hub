b9fc89350193de9028843aa63a9c85265315e280
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-06-15 13:28:56 +0200

    [factory]: Adding new test-twitter-group-generation group

---
+++ group-generators/generators/test-twitter-group-generation/index.ts
@@ -0,0 +1,36 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "twitter:apele_eth": "1",
+    };
+
+    return [
+      {
+        name: "test-twitter-group-generation",
+        timestamp: context.timestamp,
+        description: "Data group of test",
+        specs: "Hold a test",
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
