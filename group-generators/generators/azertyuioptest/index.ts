81e8a84befc7bddd97daa250f91af6c6a111be13
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-06-19 17:01:41 +0200

    [factory]: Adding new azertyuioptest group (#1916)

---
+++ group-generators/generators/azertyuioptest/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const lensProvider = new dataProviders.LensProvider();
+    
+    const lensProviderData0 = await lensProvider.getFollowers({
+      profileId: "thisisatestinthefactory.eth"
+    });
+
+    return [
+      {
+        name: "azertyuioptest",
+        timestamp: context.timestamp,
+        description: "aqw",
+        specs: "pmn",
+        data: lensProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
