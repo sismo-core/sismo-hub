2b9d279aeed9de3fedb04016f299abe24d5984c2
MartinGbz <martin.grabarz@outlook.fr>
2023-06-20 13:03:00 +0200

    fix: stani-lens-followers group + add a test group

--- group-generators/generators/0xlegion-lens-follower/index.ts
+++ group-generators/generators/0xlegion-lens-follower/index.ts
@@ -26,28 +26,23 @@ const generator: GroupGenerator = {
     // const lensProviderData0 = await LensProviderBigQuery.getFollowers({
     //   profileId: "MartinGbz"
     // });
-    // "0x4801eb5a2a6e2d04f019098364878c70a05158f1"
-    // 0x4801eB5a2A6E2D04F019098364878c70a05158F1
-    // 0x4801EB5A2A6E2D04F019098364878C70A05158F1
-
+    // // "0x4801eb5a2a6e2d04f019098364878c70a05158f1"
+    // // 0x4801eB5a2A6E2D04F019098364878c70a05158F1
+    // // 0x4801EB5A2A6E2D04F019098364878C70A05158F1
     // const count = await LensProviderBigQuery.getFollowersCount({
     //   profileId: "MartinGbz"
     // });
 
-    // console.log(count);
-
-    const lensProviderData0 = await LensProviderBigQuery.getPublicationCollectors({
-      publicationId: "0x01c960-0x10"
-    });
-    
-    const count = await LensProviderBigQuery.getPublicationCollectorsCount({
-      publicationId: "0x01c960-0x10"
-    });
+    // const lensProviderData0 = await LensProviderBigQuery.getPublicationCollectors({
+    //   publicationId: "0x01c960-0x10"
+    // });
+    // const count = await LensProviderBigQuery.getPublicationCollectorsCount({
+    //   publicationId: "0x01c960-0x10"
+    // });
     
     // const lensProviderData0 = await LensProviderBigQuery.getPublicationMirrorers({
     //   publicationId: "0x01c960-0x10"
     // });
-
     // const count = await LensProviderBigQuery.getPublicationMirrorersCount({
     //   publicationId: "0x01c960-0x10"
     // });
@@ -56,7 +51,6 @@ const generator: GroupGenerator = {
     //   publicationId: "0x10a6-0x29-DA-1dc746f6",
     //   reaction: "UPVOTE"
     // });
-
     // const count = await LensProviderBigQuery.getPublicationReactorsCount({
     //   publicationId: "0x10a6-0x29-DA-1dc746f6",
     //   reaction: "UPVOTE"
@@ -65,24 +59,16 @@ const generator: GroupGenerator = {
     // const lensProviderData0 = await LensProviderBigQuery.getPublicationCommenters({
     //   publicationId: "0x01-0x01e3"
     // });
-
     // const count = await LensProviderBigQuery.getPublicationCommentersCount({
     //   publicationId: "0x01-0x01e3"
     // });
 
-    // const lensProviderData0 = await LensProviderBigQuery.getHashtagMentioners({
-    //   hashtag: "#DeFi"
-    // });
-
-    // const count = await LensProviderBigQuery.getHashtagMentionersCount({
-    //   hashtag: "#DeFi"
-    // });
-
-    // const lensProvider = new dataProviders.LensProvider();
-
-    // const lensProviderData0 = await lensProvider.getFollowers({
-    //   profileId: "thisisatestinthefactory.eth"
-    // });
+    const lensProviderData0 = await LensProviderBigQuery.getHashtagMentioners({
+      hashtag: "#DeFi"
+    });
+    const count = await LensProviderBigQuery.getHashtagMentionersCount({
+      hashtag: "#DeFi"
+    });
 
     console.log("lensProviderData0", lensProviderData0);
 
