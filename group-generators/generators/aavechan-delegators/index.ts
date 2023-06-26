7d76a08d0d717275737328844f3d9f1c2eef48b3
MartinGbz <martin.grabarz@outlook.fr>
2023-06-26 13:19:55 +0200

    fix: switch aavechan generation frequency from daily to once

--- group-generators/generators/aavechan-delegators/index.ts
+++ group-generators/generators/aavechan-delegators/index.ts
@@ -12,7 +12,7 @@ import {
 } from "topics/group-generator";
 
 const generator: GroupGenerator = {
-  generationFrequency: GenerationFrequency.Daily,
+  generationFrequency: GenerationFrequency.Once,
 
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
     const bigQueryProvider = new BigQueryProvider();
