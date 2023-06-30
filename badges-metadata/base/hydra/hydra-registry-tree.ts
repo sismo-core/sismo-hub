f097029df87e4193bf27993b3e7daa789eff1920
bigq <99990370+yum0e@users.noreply.github.com>
2023-06-30 11:06:44 +0200

    feat: keep the last valid root onchain alongisde the new registered root (#1934)
    + add 48h instead of 24h limit timestamp

--- badges-metadata/base/hydra/hydra-registry-tree.ts
+++ badges-metadata/base/hydra/hydra-registry-tree.ts
@@ -82,7 +82,7 @@ export abstract class HydraRegistryTreeBuilder
     return hash;
   }
 
-  public async removeOnChain(identifierToKeep: string): Promise<void> {
+  public async removeOnChain(newIdentifierToKeep: string, currentIdentifierToKeep: string): Promise<void> {
     const availableData = await this._availableDataStore.search({
       registryTreeName: this.name,
       network: this.network,
@@ -91,9 +91,11 @@ export abstract class HydraRegistryTreeBuilder
     // Do not delete roots that are less than 48h old
     const limitTimestamp = Math.floor(Date.now() / 1000) - 3600 * 48;
     for (const data of availableData) {
-      // Do not unregister on chain if root has not changed
+      // Do not unregister the new registered root
+      // Do not unregister the last valid root
+      const doesIdentifierNeedsToBeKept = ((data.identifier === newIdentifierToKeep) || (data.identifier === currentIdentifierToKeep))
       if (
-        identifierToKeep != data.identifier &&
+        !doesIdentifierNeedsToBeKept  &&
         data.timestamp < limitTimestamp
       ) {
         this._logger.info(`Unregister previous root ${data.identifier}...`);
