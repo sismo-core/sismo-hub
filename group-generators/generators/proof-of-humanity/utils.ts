c93b65688ac05ee739eae5daa8a02e0b59c0413f
MartinGbz <martin.grabarz@outlook.fr>
2023-07-03 10:57:11 +0200

    fix: add some comments to utils file

--- group-generators/generators/proof-of-humanity/utils.ts
+++ group-generators/generators/proof-of-humanity/utils.ts
@@ -3,6 +3,14 @@ import { ethers } from "ethers";
 import { JsonRpcProvider } from "@group-generators/helpers/data-providers/json-rpc";
 import { FetchedData } from "topics/group";
 
+// This file is a helper to test the PoH generator
+// These functions are not used in the generator itself but are useful to check if the accounts fetched are effectively registered 
+
+/**
+ * Verify onchain if an address is registered on PoH
+ * @param address group
+ * @returns a boolean indicating if the address is registered
+ */
 const isRegistred = async (address: string): Promise<FetchedData> => {
   const pohContractAddress = "0xC5E9dDebb09Cd64DfaCab4011A0D5cEDaf7c9BDb"
   const jsonRPCProvider = new JsonRpcProvider(process.env.JSON_RPC_URL);
@@ -12,6 +20,12 @@ const isRegistred = async (address: string): Promise<FetchedData> => {
   return isRegistered
 };
 
+/**
+ * Verify onchain if a group of addresses are registered on PoH 
+ * @param addresses group
+ * @param max the number of addresses to be tested
+ * @returns ratio of registered addresses
+ */
 export const areRegistred = async (addresses: FetchedData, max: number): Promise<number> => {
   let count = 0;
   let realCount = 0;
@@ -35,6 +49,12 @@ export const areRegistred = async (addresses: FetchedData, max: number): Promise
   return count/max;
 }
 
+/**
+ * Verify onchain if a group of addresses are registered on PoH
+ * @param addresses group
+ * @param max the number of randomly selected addresses to be tested
+ * @returns ratio of registered addresses
+ */
 export const areRegistredWithRandomness = async (addresses: FetchedData, max: number): Promise<number> => {
   let count = 0;
   let realCount = 0;
