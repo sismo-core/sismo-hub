1cd586f14b75f08905842e542a15339567436a54
gabin54 <59560717+gabin54@users.noreply.github.com>
2023-07-03 19:39:54 +0200

    feat: add world id dev group generator (#1976)

---
+++ group-generators/generators/world-id-dev/index.ts
@@ -0,0 +1,32 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+const generator: GroupGenerator = {
+  generationFrequency: GenerationFrequency.Once,
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const jsonListData0 = {
+      "sismo-connect-app(appid=0x3bcdab2ad3caddb11b90b02bde258f6b):0x244621f0aa0bc878f70524e274dd385787eda22d90f24d6aa8eba9b5f04004d3": "1",
+      "sismo-connect-app(appid=0x3bcdab2ad3caddb11b90b02bde258f6b):0x0c44ef638a384af02802b2c5322970ae05ed7ddc09b2b9d804b6acecc2b47072": "1",
+    };
+
+    return [
+      {
+        name: "world-id-dev",
+        timestamp: context.timestamp,
+        description: "World ID dev group",
+        specs: "",
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.SybilResistance],
+      },
+    ];
+  },
+};
+
+export default generator;
