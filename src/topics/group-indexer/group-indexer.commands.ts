f87781673fbeac0a85a0bb117686627ca421cc3e
Alexandru Pele <pelealexandru@gmail.com>
2023-07-05 12:32:51 +0200

    add group name argument for command

--- src/topics/group-indexer/group-indexer.commands.ts
+++ src/topics/group-indexer/group-indexer.commands.ts
@@ -1,8 +1,9 @@
 import { SismoHubCmd } from "cli/command";
 
-export const indexGroup = async (): Promise<void> => {
-  console.log("indexing groups...");
+export const indexGroup = async (groupName: string): Promise<void> => {
+  console.log(`indexing group ${groupName}`);
 };
 
 export const indexGroupCmd = new SismoHubCmd("index-group");
+indexGroupCmd.arguments("group-name");
 indexGroupCmd.action(indexGroup);
