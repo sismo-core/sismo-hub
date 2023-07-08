58997bb52319c13427d96a7c8ae8d51c73afae53
leo Sayous <leo@sismo.io>
2023-07-08 13:14:02 +0200

    feat: save json data group in a pretty format

--- src/file-store/file-store.ts
+++ src/file-store/file-store.ts
@@ -1,5 +1,14 @@
 import { Api } from "api";
 
+export type WriteOptions = {
+  json?: boolean;
+  pretty?: boolean;
+};
+
+export type ReadOptions = {
+  json?: boolean;
+};
+
 export abstract class FileStore {
   protected prefix: string;
 
@@ -8,13 +17,9 @@ export abstract class FileStore {
   }
 
   public abstract exists(filename: string): Promise<boolean>;
-  public abstract read(filename: string): Promise<any>;
+  public abstract read(filename: string, options?: ReadOptions): Promise<any>;
   public abstract url(filename: string): string;
-  public abstract write(
-    filename: string,
-    data: any,
-    json?: boolean
-  ): Promise<void>;
+  public abstract write(filename: string, data: any, options?: WriteOptions): Promise<void>;
   public abstract delete(filename: string): Promise<void>;
   public abstract registerRoutes(): (api: Api) => Promise<void>;
 }
