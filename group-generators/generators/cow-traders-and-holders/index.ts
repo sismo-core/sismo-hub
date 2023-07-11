5cc181d593c859ea62de4a61d163af6d9600aa66
MartinGbz <martin.grabarz@outlook.fr>
2023-07-11 15:05:08 +0200

    fix: cowswap groups (#2010)

--- group-generators/generators/cow-traders-and-holders/index.ts
+++ group-generators/generators/cow-traders-and-holders/index.ts
@@ -1,5 +1,3 @@
-
-import { BigNumber } from "ethers";
 import { dataOperators } from "@group-generators/helpers/data-operators";
 import { UnionOption } from "@group-generators/helpers/data-operators/union";
 import { ValueType, GroupWithData, GroupStore } from "topics/group";
@@ -27,17 +25,7 @@ const generator: GroupGenerator = {
     const dataUnion = dataOperators.Union([ 
       await cowHoldersGroupLatest.data(),
       await cowTradersGroupLatest.data(),
-    ], UnionOption.Min);
-
-    // Switch all the value 3 to 1 and 1 to 3
-    const dataUnionSwitched = {...dataUnion}
-    Object.entries(dataUnion).forEach(([address, value]) => {
-      if (BigNumber.from(value).eq(3)) {
-        dataUnionSwitched[address] = BigNumber.from(1).toString();
-      } else if (BigNumber.from(value).eq(1)) {
-        dataUnionSwitched[address] = BigNumber.from(3).toString();
-      }
-    });
+    ], UnionOption.Max);
 
     return [
       {
@@ -45,7 +33,7 @@ const generator: GroupGenerator = {
         timestamp: context.timestamp,
         description: "Data group of COW Token Holders & COW Swap Traders",
         specs: "Networks: Ethereum mainnet, Gnosis chain. Token Holders, Snapshot date on 2023-07-01. Tokens: COW, vCOW. Value 1 Top 50% of holders - Value 2 Top 30% of holders - Value 3 Top 15% of holders. Swap Traders, Time period: 2023-01-01 - 2023-07-01. Value 1 Traded on CoW Swap 2 times or more - Value 2 Traded on CoW Swap 6 times or more - Value 3 Traded on CoW Swap 37 times or more.",
-        data: dataUnionSwitched,
+        data: dataUnion,
         valueType: ValueType.Score,
         tags: [],
       },
