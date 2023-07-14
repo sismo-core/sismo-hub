36d3755a6e9f8b92fd4a741f96494ab62cc71399
leo Sayous <leo@sismo.io>
2023-07-14 12:16:38 +0200

    fix: gitcoin passport holder group

--- group-generators/generators/gitcoin-passport-holders/index.ts
+++ group-generators/generators/gitcoin-passport-holders/index.ts
@@ -1,32 +1,27 @@
 import { dataProviders } from "@group-generators/helpers/data-providers";
 import { ApiConfig } from "@group-generators/helpers/data-providers/rest-api";
 import { Tags, ValueType, GroupWithData, FetchedData, AccountSource } from "topics/group";
-import {
-  GenerationContext,
-  GenerationFrequency,
-  GroupGenerator,
-} from "topics/group-generator";
+import { GenerationContext, GenerationFrequency, GroupGenerator } from "topics/group-generator";
 
 const generator: GroupGenerator = {
   generationFrequency: GenerationFrequency.Daily,
 
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
-
     type Passport = {
-      address: string,
-      score: string,
-      status: string,
-      last_score_timestamp: string,
+      address: string;
+      score: string;
+      status: string;
+      last_score_timestamp: string;
       evidence: {
-        type: string,
-        success: boolean,
-        rawScore: string,
-        threshold: string
-      },
-      error: any
+        type: string;
+        success: boolean;
+        rawScore: string;
+        threshold: string;
+      };
+      error: any;
     };
 
-    const evmAddressRegEx = new RegExp("^0x[a-fA-F0-9]{40}$")
+    const evmAddressRegEx = new RegExp("^0x[a-fA-F0-9]{40}$");
 
     const restProvider = new dataProviders.RestProvider();
     const gitcoinPassportHolders: FetchedData = {};
@@ -40,20 +35,26 @@ const generator: GroupGenerator = {
       },
     };
 
-    const passports: Passport[] = await restProvider.fetchData(apiConfig) as any as Passport[];
+    const passports: Passport[] = (await restProvider.fetchData(apiConfig)) as any as Passport[];
 
-    passports.forEach((passport: Passport) => {
-      if(passport.status == "DONE" && evmAddressRegEx.test(passport.address)) {
-        gitcoinPassportHolders[passport.address] = Math.floor(Number(passport.evidence.rawScore)).toString();
+    for (const passport of passports) {
+      if (evmAddressRegEx.test(passport.address)) {
+        if (!passport?.evidence?.rawScore) {
+          continue;
+        }
+        gitcoinPassportHolders[passport.address] = Math.floor(
+          Number(passport.evidence.rawScore)
+        ).toString();
       }
-    });
+    }
 
     return [
       {
         name: "gitcoin-passport-holders",
         timestamp: context.timestamp,
         description: "Data Group of all addresses that own a Gitcoin Passport",
-        specs: "Contains all addresses that own a Gitcoin Passport. The value of each group member corresponds to their Gitcoin Passport score.",
+        specs:
+          "Contains all addresses that own a Gitcoin Passport. The value of each group member corresponds to their Gitcoin Passport score.",
         data: gitcoinPassportHolders,
         valueType: ValueType.Score,
         accountSources: [AccountSource.ETHEREUM],
