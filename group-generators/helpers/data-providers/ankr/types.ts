1ebcc0a62b8f78a8d36c9a128fa295d3aa47fd6c
MartinGbz <martin.grabarz@outlook.fr>
2023-07-21 14:50:50 +0200

    improve-ankr-provider

--- group-generators/helpers/data-providers/ankr/types.ts
+++ group-generators/helpers/data-providers/ankr/types.ts
@@ -3,7 +3,7 @@ export type TokenInfo = {
   address: string;
 };
 
-export enum Chains {
+export enum SupportedNetwork {
   ETH = "eth",
   BSC = "bsc",
   FANTOM = "fantom",
@@ -16,6 +16,35 @@ export enum Chains {
   POLYGON_MUMBAI = "polygon_mumbai",
   AVALANCHE_FUJI = "avalanche_fuji"
 }
+
+export const fromStringToSupportedNetwork = (network: string): SupportedNetwork => {
+  switch (network) {
+    case "eth" || "1":
+      return SupportedNetwork.ETH;
+    case "bsc" || "56":
+      return SupportedNetwork.BSC;
+    case "fantom" || "250":
+      return SupportedNetwork.FANTOM;
+    case "avalanche" || "43114":
+      return SupportedNetwork.AVALANCHE;
+    case "polygon" || "137":
+      return SupportedNetwork.POLYGON;
+    case "arbitrum" || "42161":
+      return SupportedNetwork.ARBITRUM;
+    case "syscoin" || "57":
+      return SupportedNetwork.SYSCOIN;
+    case "optimism" || "10":
+      return SupportedNetwork.OPTIMISM;
+    case "eth_goerli" || "5":
+      return SupportedNetwork.ETH_GOERLI;
+    case "polygon_mumbai" || "80001":
+      return SupportedNetwork.POLYGON_MUMBAI;
+    case "avalanche_fuji" || "43113":
+      return SupportedNetwork.AVALANCHE_FUJI;
+    default:
+      throw new Error(`Unsupported network named ${network}`);
+  }
+};
 
 export type AnkrTokenQueryResponse = {
   error: Record<string, unknown>;
