64862d8e9ea2445fe067bcb5ac74b7384f90863e
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-07-22 16:08:19 +0200

    [factory]: Adding new lens-collaboration-able-post group (#2085)
    Co-authored-by: leo21.sismo.eth <leo@sismo.io>

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -348,6 +348,7 @@ import layerZeroUser from "./layer-zero-user";
 import lazybaer from "./lazybaer";
 import leidream from "./leidream";
 import lens50BestFollowed from "./lens-50-best-followed";
+import lensCollaborationAblePost from "./lens-collaboration-able-post";
 import lensCollaborationTool from "./lens-collaboration-tool";
 import lensFollowers from "./lens-followers";
 import lensFrens from "./lens-frens";
@@ -987,6 +988,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "kunal-data-group": kunalDataGroup,
   "kunal-gits": kunalGits,
   "kunal-group-2": kunalGroup2,
+  "lens-collaboration-able-post": lensCollaborationAblePost,
   "lens-collaboration-tool": lensCollaborationTool,
   "lens-frens": lensFrens,
   "lens-profile-holders": lensProfileHolders,
