0f5ddb4e4d08fc8bcfb34cbc842bdecdc11d27f4
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-07-22 14:45:02 +0200

    [factory]: Adding new holdingc10eas group

---
+++ group-generators/generators/holdingc10eas/index.ts
@@ -0,0 +1,36 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Daily,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x573E0AAb5A6c54812cbE1f928c0E138058a70e03": "1",
+    };
+
+    return [
+      {
+        name: "holdingc10eas",
+        timestamp: context.timestamp,
+        description: "test",
+        specs: "test",
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
