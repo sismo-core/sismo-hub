c6063df43da5f907105ff0cfcbde535ebff04a12
MartinGbz <martin.grabarz@outlook.fr>
2023-07-25 18:10:47 +0200

    fix: hive groups + remove hive provider from the factory + prettify files (#2123)

--- group-generators/generators/ens-supporters/index.ts
+++ group-generators/generators/ens-supporters/index.ts
@@ -1,16 +1,6 @@
 import { dataProviders } from "@group-generators/helpers/data-providers";
-import {
-  Tags,
-  ValueType,
-  GroupWithData,
-  FetchedData,
-  AccountSource,
-} from "topics/group";
-import {
-  GenerationContext,
-  GenerationFrequency,
-  GroupGenerator,
-} from "topics/group-generator";
+import { Tags, ValueType, GroupWithData, FetchedData, AccountSource } from "topics/group";
+import { GenerationContext, GenerationFrequency, GroupGenerator } from "topics/group-generator";
 
 const generator: GroupGenerator = {
   generationFrequency: GenerationFrequency.Once,
@@ -20,12 +10,11 @@ const generator: GroupGenerator = {
     const ensProvider = new dataProviders.EnsProvider();
 
     // we query the first 10k influencers on Hive in the Ethereum Cluster with at least one follower
-    const hiveInfluencersEthereum =
-      hiveProvider._getInfluencersFromClusterWithMinimumFollowers(
-        {clusterName: "Ethereum"},
-        10000,
-        0
-      );
+    const hiveInfluencersEthereum = hiveProvider._getInfluencersFromClusterWithMinimumFollowers(
+      { clusterName: "Ethereum" },
+      10000,
+      0
+    );
     // regex to find all ens names on Hive
     const regex = /[A-Za-z0-9._-]+\.eth/;
     const ethInfluencerFromHive = [];
@@ -40,16 +29,15 @@ const generator: GroupGenerator = {
 
     // resolve ens addresses by batches of 100 ens users
     // add it to data with a value of 1
-    const data: FetchedData = await ensProvider.getAllAddresses(
-      ethInfluencerFromHive
-    );
+    const data: FetchedData = await ensProvider.getAllAddresses(ethInfluencerFromHive);
 
     return [
       {
         name: "ens-supporters",
         timestamp: context.timestamp,
         description: "Be part of the most reputable ENS domain accounts on Twitter",
-        specs: "Be part of the first 10k Ethereum Twitter Influencer listed on Hive.one that added their .eth name in their username",
+        specs:
+          "Be part of the first 10k Ethereum Twitter Influencer listed on Hive.one that added their .eth name in their username",
         data: data,
         accountSources: [AccountSource.ETHEREUM],
         valueType: ValueType.Score,
