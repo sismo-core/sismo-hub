d743c8a2fea81684daec4fbc84aa8ee7fecbc084
bigq <yum0e@protonmail.com>
2023-08-21 10:40:23 +0200

    fix: quotes

--- group-generators/generators/potato-nft-minters/index.ts
+++ group-generators/generators/potato-nft-minters/index.ts
@@ -1,20 +1,12 @@
-
 import { Tags, ValueType, GroupWithData } from "topics/group";
-import {
-  GenerationContext,
-  GenerationFrequency,
-  GroupGenerator,
-} from "topics/group-generator";
+import { GenerationContext, GenerationFrequency, GroupGenerator } from "topics/group-generator";
 
 // Generated from factory.sismo.io
 
 const generator: GroupGenerator = {
-  
   generationFrequency: GenerationFrequency.Daily,
-  
+
   generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
-  
-    
     const jsonListData0 = {
       "github:YasuBlockchain": "1",
       "github:0xheartcode": "1",
@@ -24,7 +16,7 @@ const generator: GroupGenerator = {
       {
         name: "potato-nft-minters",
         timestamp: context.timestamp,
-        description: "Data group of minters of the "potato" NFT",
+        description: 'Data group of minters of the "potato" NFT',
         specs: "A very useful description",
         data: jsonListData0,
         valueType: ValueType.Score,
