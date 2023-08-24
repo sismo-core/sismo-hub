8f8dcaebaea55ec065dea3ee19f018b494647204
bigq <yum0e@protonmail.com>
2023-08-24 16:15:17 +0200

    fix: double quotes

--- group-generators/generators/interacted-with-tosco042-eth/index.ts
+++ group-generators/generators/interacted-with-tosco042-eth/index.ts
@@ -25,7 +25,7 @@ const generator: GroupGenerator = {
         name: "interacted-with-tosco042-eth",
         timestamp: context.timestamp,
         description: "Data group of wallets that interacted with tosco042.eth",
-        specs: "The mentioned data group comprises the wallets that have engaged in some form of interaction, such as transferring funds, executing smart contracts, or participating in decentralized applications, with the Ethereum address "tosco042.eth." This data could potentially be analyzed or studied for various purposes, such as understanding the network's activity, tracking transactions, or conducting research on Ethereum-related behaviors.",
+        specs: "The mentioned data group comprises the wallets that have engaged in some form of interaction, such as transferring funds, executing smart contracts, or participating in decentralized applications, with the Ethereum address 'tosco042.eth.' This data could potentially be analyzed or studied for various purposes, such as understanding the network's activity, tracking transactions, or conducting research on Ethereum-related behaviors.",
         data: jsonListData0,
         valueType: ValueType.Score,
         tags: [Tags.Factory],
