270b9243e5aadb686cab2914146dcd9649a7ad13
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-10-07 00:29:34 +0200

    [factory]: Adding new truchottestgroup group (#2195)

--- group-generators/generators/index.ts
+++ group-generators/generators/index.ts
@@ -770,6 +770,7 @@ import tradingviewAlertConnectorStargazers from "./tradingview-alert-connector-s
 import treasure from "./treasure";
 import troix123 from "./troix123";
 import trove from "./trove";
+import truchottestgroup from "./truchottestgroup";
 import tuleepDrop from "./tuleep-drop";
 import tundraPlayersClub from "./tundra-players-club";
 import turkiyeEarthquakeReliefDao from "./turkiye-earthquake-relief-dao";
@@ -1638,6 +1639,7 @@ export const groupGenerators: GroupGeneratorsLibrary = {
   "treasure": treasure,
   "troix123": troix123,
   "trove": trove,
+  "truchottestgroup": truchottestgroup,
   "tuleep-drop": tuleepDrop,
   "tundra-players-club": tundraPlayersClub,
   "turkiye-earthquake-relief-dao": turkiyeEarthquakeReliefDao,
