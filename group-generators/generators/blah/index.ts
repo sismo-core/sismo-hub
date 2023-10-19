808e29680788f8d6bbb61430dfe22b4e167989ad
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-10-19 12:44:15 +0200

    [factory]: Adding new blah group (#2232)

---
+++ group-generators/generators/blah/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Daily,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const restProvider = new dataProviders.RestProvider();
+    
+    const restProviderData0 = await restProvider.getAccountsFromAPI({
+      url: "https://rosca.free.beeceptor.com/todos"
+    });
+
+    return [
+      {
+        name: "blah",
+        timestamp: context.timestamp,
+        description: "blah",
+        specs: "blah",
+        data: restProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
