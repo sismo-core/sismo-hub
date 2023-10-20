f90d9fec39ae87daac13a2669d6067e826463a77
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-10-20 11:05:53 +0200

    [factory]: Adding new poap-for-push-roadshow group

---
+++ group-generators/generators/poap-for-push-roadshow/index.ts
@@ -0,0 +1,38 @@
+
+import { dataProviders } from "@group-generators/helpers/data-providers";
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Once,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    const poapSubgraphProvider = new dataProviders.PoapSubgraphProvider();
+    
+    const poapSubgraphProviderData0 = await poapSubgraphProvider.queryEventsTokenOwners({
+      eventIds: [ "82214" ]
+    });
+
+    return [
+      {
+        name: "poap-for-push-roadshow",
+        timestamp: context.timestamp,
+        description: "POAP",
+        specs: "POAP",
+        data: poapSubgraphProviderData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
