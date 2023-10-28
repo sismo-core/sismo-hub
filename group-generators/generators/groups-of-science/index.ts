15d7f07274e2858a7a5764b1cf00bc678d361f51
Sismobot <94460392+sismobot@users.noreply.github.com>
2023-10-28 09:45:17 +0200

    [factory]: Adding new groups-of-science group

---
+++ group-generators/generators/groups-of-science/index.ts
@@ -0,0 +1,36 @@
+
+import { Tags, ValueType, GroupWithData } from "topics/group";
+import {
+  GenerationContext,
+  GenerationFrequency,
+  GroupGenerator,
+} from "topics/group-generator";
+
+// Generated from factory.sismo.io
+
+const generator: GroupGenerator = {
+  
+  generationFrequency: GenerationFrequency.Daily,
+  
+  generate: async (context: GenerationContext): Promise<GroupWithData[]> => {
+  
+    
+    const jsonListData0 = {
+      "0x3DdFBcF4375d2cFa36A71819ca5F6f0F018255eD": "1",
+    };
+
+    return [
+      {
+        name: "groups-of-science",
+        timestamp: context.timestamp,
+        description: "Data group of science people",
+        specs: "NA",
+        data: jsonListData0,
+        valueType: ValueType.Score,
+        tags: [Tags.Factory],
+      },
+    ];
+  },
+};
+
+export default generator;
